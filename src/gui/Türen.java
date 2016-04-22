8044279bf8b0f8428c289712d845437af4e9850f
Nina-Egger <nina.egger@netabc.ch>
2016-04-22 08:03:04 +0200

    Grundfenster
    Das Grundfenster ist schon mal da.

--- src/gui/Türen.java
+++ src/gui/Türen.java
@@ -1,6 +1,36 @@
 package gui;
 
-public class Türen {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class Türen extends Application{
+	
+	Stage window;
+	Scene doorScene;
+	Button zurückButton;
+
+	public static void main(String[] args) {
+	launch(args);
+	}
+
+	public void start(Stage primaryStage) throws Exception {
+	window = primaryStage;
+	window.setTitle("Türen/Themen");
+
+	zurückButton = new Button("zurück");
+	
+	VBox layout = new VBox(10);
+	layout.setPadding(new Insets(10));
+	layout.getChildren().addAll(zurückButton);
+	
+	Scene doorScene = new Scene(layout, 800, 450);
+	window.setScene(doorScene);
+	window.show();
+		
+	}
+	
 }
-// hier kommt das Türen/Themen Fenster (noch in bearbeitung (Nina) 

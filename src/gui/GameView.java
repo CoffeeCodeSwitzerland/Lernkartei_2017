21f6d1329501d39870668e450d46daf465b2cdea
hlucca <hlucca@gmx.net>
2016-04-25 09:43:54 +0200

    Neuer Gameview mit eigener CSS Darstellung (bg-img, fancytext)

--- src/gui/GameView.java
+++ src/gui/GameView.java
@@ -6,11 +6,21 @@ import debug.Debugger;
 import javafx.animation.PauseTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.SequentialTransition;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -30,28 +40,31 @@ public class GameView extends View
 	{
 		super (setName, window);
 		myController = controller;
-		Button b = new Button("Starting game (please wait)...");
 		BorderPane bp = new BorderPane();
-		bp.setCenter(b);
+		Text t = new Text("Starting game (please wait)...");
+		t.setId("fancytext");
+		Button b = new Button("Zurück...");
+		b.setOnAction(e -> controller.show("mainview"));
+
+		// Erstellt Layout
+		VBox tempVBox = new VBox();
+		tempVBox.setPadding(new Insets(10));
+		tempVBox.setSpacing(10);
+		tempVBox.setAlignment(Pos.CENTER);
+		tempVBox.getChildren().addAll(t, b);
+		bp.setCenter(tempVBox);
+		bp.setId("gamebg");
 		this.setScene(new Scene(bp, 800, 450));
 	}
 
 	public void show () {
+
+		// TODO funktioniert noch nicht richtig...
+		// - keine Umschaltung auf dieses Game BS, wenn Spiel aufgerufen wird
+		// - und auch kein Zurück zum Main-menü möglich (Swing ist schuld)
+		//
 		super.show();
-//		Rectangle rect = new Rectangle (100, 40, 100, 100);
-//	     rect.setArcHeight(50);
-//	     rect.setArcWidth(50);
-//	     rect.setFill(Color.VIOLET);
-//	 
-//	     RotateTransition rt = new RotateTransition(Duration.millis(3000), rect);
-//	     rt.setByAngle(180);
-//	     rt.setCycleCount((int) 4f);
-//	     rt.setAutoReverse(true);
-//		 new SequentialTransition (
-//		         new PauseTransition(Duration.millis(2000)), null // wait a second
-//		 );
 		scrollyv8.ScrollyV8.main(null);
 		
-		myController.show("mainview");
 	}
 }

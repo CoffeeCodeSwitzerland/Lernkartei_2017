a3551dabbea8b7723eff2ddb1318b17ca7a557f0
rogerschneiter <roger.schneiter@gmail.com>
2016-04-27 09:41:43 +0200

    UserDatabase geupdated
    UserDatabase für Joel's Login überarbeitet und vereinfacht.
    Funktionalität bis auf Delete fertig gestellt.

--- src/database/UserDatabase.java
+++ src/database/UserDatabase.java
@@ -1,374 +1,230 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import sqlite.*;
 
-import sqlite.Attributes;
-import sqlite.Table;
 
 public class UserDatabase {
 
-	// Variabeldeklaration
+	/**
+	 * Fügt der Datenbank einen neuen User hinzu
+	 * 
+	 * @param values
+	 *            --> String Array mit 3 Werten. Username, E-Mail, Passwort
+	 * @param teacher
+	 *            --> Boolean: true = Lehrer, false = Schüler
+	 */
 
-	private boolean teachPossible = true;
-	private boolean pwdCorr = true;
-	private boolean studPossible = true;
-	private Table teacher;
-	private Table student;
-	private String selTable;
-	private String selUsern;
-	private Connection c = null;
-	private Statement stmt = null;
-	private String separator = ":::";
-	private String passwd = "";
-	private String attributes = "";
-	private String insValues = "";
-	private final ArrayList<String> values = new ArrayList<String>();
+	public void newUser (String[] values, boolean teacher) {
 
-	// Konstruktor, erstellt Tabellen in der Datenbank namens: test.db
+		Connection c = null;
+		Statement stmt = null;
 
-	public UserDatabase() {
-
-		// Erstellen der Tabellen, Attribute (Spaltenname, Datentyp, NotNull)
-
-		Attributes usern = new Attributes("Username", "TEXT", true);
-		Attributes email = new Attributes("E-Mail", "TEXT", true);
-		Attributes passwd = new Attributes("Password", "TEXT", true);
-
-		teacher = new Table("Lehrer");
-		teacher.addAttrs(usern);
-		teacher.addAttrs(email);
-		teacher.addAttrs(passwd);
-
-		student = new Table("Lernender");
-		student.addAttrs(usern);
-		student.addAttrs(email);
-		student.addAttrs(passwd);
-
-	}
-
-	public boolean checkPossible(String values) {
+		String url = "jdbc:mariadb://192.168.3.150:3306/userdb";
+		String username = "prototyp";
+		String password = "prototyp";
+		String driver = "com.mysql.jdbc.Driver";
 
 		try {
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:test.db");
-			c.setAutoCommit(false);
-
+			Class.forName(driver);
+			c = DriverManager.getConnection(url, username, password);
 			stmt = c.createStatement();
 
-			// Abfrage, ob ein Lehrer besteht
+			String studValues = "";
 
-			ResultSet rsTeacher = stmt.executeQuery("SELECT * FROM " + teacher.getName());
+			for (int i = 0; i < values.length; i++) {
 
-			if (!rsTeacher.next()) {
+				if (i > values.length - 1) {
 
-				while (rsTeacher.next()) {
+					studValues += "'" + values[i] + "',";
 
-					String[] splited = values.split(separator);
+				}
+				else {
 
-					for (int i = 0; i < splited.length - 1; i++) {
-
-						if (splited[i].toUpperCase()
-								.equals(rsTeacher.getString(teacher.Attrs.get(i).getName()).toUpperCase())) {
-
-							teachPossible = false;
-							rsTeacher.close();
-
-						}
-
-					}
+					studValues += "'" + values[i] + "'";
 
 				}
 
-			} else {
-
-				teachPossible = true;
-
 			}
 
-			rsTeacher.close();
+			String teachValues = "";
 
-			// Abfrage, ob ein Schüler besteht
+			for (int i = 0; i < values.length; i++) {
 
-			ResultSet rsStudent = stmt.executeQuery("SELECT * FROM " + student.getName());
+				if (i > values.length - 1) {
 
-			if (!rsStudent.next()) {
+					studValues += "'" + values[i] + "',";
 
-				while (rsStudent.next()) {
+				}
+				else {
 
-					String[] splited = values.split(separator);
-
-					for (int i = 0; i < splited.length - 1; i++) {
-
-						if (splited[i].equals(rsStudent.getString(student.Attrs.get(i).getName()))) {
-
-							studPossible = false;
-							rsStudent.close();
-
-						}
-
-					}
+					studValues += "'" + values[i] + "'";
 
 				}
 
-			} else {
-
-				studPossible = true;
-
 			}
 
-			rsStudent.close();
+			String usedb = "USE " + "userdb";
+			stmt.executeQuery(usedb);
+
+			System.out.println("Opened database successfully");
+
+			String sql1 = "CREATE TABLE IF NOT EXISTS Teachers " +
+					"(PK_Lehrer INT PRIMARY KEY AUTO_INCREMENT," +
+					" Username TEXT NOT NULL," +
+					" Email TEXT NOT NULL," +
+					" Password TEXT" + ")";
+
+			System.out.println(sql1);
+			stmt.executeUpdate(sql1);
+			System.out.println("Lehrertabelle erstellt!");
+
+			String sql2 = "CREATE TABLE IF NOT EXISTS Students " +
+					"(PK_Lehrer INT PRIMARY KEY AUTO_INCREMENT," +
+					" Username TEXT NOT NULL," +
+					" Email TEXT NOT NULL," +
+					" Password TEXT" + ")";
+
+			System.out.println(sql2);
+			stmt.executeUpdate(sql2);
+			System.out.println("Schülertabelle erstellt!");
+
+			if (teacher) {
+
+				String newTeach = "INSERT INTO Teachers (Username, Email, Password) VALUES (" + teachValues + ")";
+				System.out.println(newTeach);
+				System.out.println("Teacher sucessfully added!");
+
+			}
+			else if (!teacher) {
+
+				String newStud = "INSERT INTO Students (Username, Email, Password) VALUES (" + studValues + ")";
+				System.out.println(newStud);
+				System.out.println("Student sucessfully added!");
+
+			}
 
 			stmt.close();
 			c.close();
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
 		}
 
-		if (!teachPossible | !studPossible) {
+	}
 
-			return false;
+	/**
+	 * Überprüft ob ein Wert bereits vorhanden ist
+	 * 
+	 * @param values
+	 *            --> String Array mit 2 Werten, Username und Email
+	 * @return --> Retourniert true, wenn möglich, sonst false
+	 */
 
-		} else {
+	public boolean checkPossible (String[] values) {
 
-			return true;
+		boolean possible = true;
+		boolean noTeacher = true;
+		boolean noStudent = true;
+
+		// Überprüfen ob ein Lehrer mit dem Username oder Email vorhanden ist
+
+		Connection c = null;
+		Statement stmt = null;
+
+		String url = "jdbc:mariadb://192.168.3.150:3306/userdb";
+		String username = "prototyp";
+		String password = "prototyp";
+		String driver = "com.mysql.jdbc.Driver";
+
+		try {
+			Class.forName(driver);
+			c = DriverManager.getConnection(url, username, password);
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+
+			ResultSet rsUsern = stmt.executeQuery("SELECT Username FROM Teacher WHERE Username = " + values[0]);
+			ResultSet rsEmail = stmt.executeQuery("SELECT Email FROM Teacher WHERE Email = " + values[1]);
+
+			if (rsUsern.next() || rsEmail.next()) {
+
+				noTeacher = false;
+
+			}
+
+			rsUsern.close();
+			rsEmail.close();
+			stmt.close();
+			c.close();
+		}
+		catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+
+		// Überprüfen ob ein Schüler mit dem Username oder Email vorhanden ist
+
+		try {
+			Class.forName(driver);
+			c = DriverManager.getConnection(url, username, password);
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+
+			ResultSet rsUsern = stmt.executeQuery("SELECT Username FROM Student WHERE Username = " + values[0]);
+			ResultSet rsEmail = stmt.executeQuery("SELECT Email FROM Student WHERE Email = " + values[1]);
+
+			if (rsUsern.next() || rsEmail.next()) {
+
+				noStudent = false;
+
+			}
+
+			rsUsern.close();
+			rsEmail.close();
+			stmt.close();
+			c.close();
+		}
+		catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+
+		// Wenn keine Lehrer und keine Schüler mit passendem Username oder Email
+		// vorhanden ist, ist es möglich
+
+		if (noTeacher && noStudent) {
+
+			possible = true;
 
 		}
+		else {
+
+			possible = false;
+
+		}
+
+		return possible;
 
 	}
 
-	public void insert(String valuee, boolean person) {
-
-		if (person) {
-
-			for (int i = 0; i < valuee.split(separator).length; i++) {
-
-				values.add(valuee.split(separator)[i]);
-
-			}
-
-			try {
-				Class.forName("org.sqlite.JDBC");
-				c = DriverManager.getConnection("jdbc:sqlite:test.db");
-				c.setAutoCommit(false);
-				stmt = c.createStatement();
-
-				// Attributes
-
-				for (int j = 0; j < teacher.Attrs.size(); j++) {
-
-					if (j < teacher.Attrs.size() - 1) {
-
-						attributes += teacher.Attrs.get(j).getName() + ",";
-
-					} else {
-
-						attributes += teacher.Attrs.get(j).getName();
-
-					}
-
-				}
-
-				// Values
-
-				for (int k = 0; k < values.size(); k++) {
-
-					if (k < values.size() - 1) {
-
-						if (teacher.Attrs.get(k).getDatatype() == "INTEGER") {
-
-							insValues += values.get(k) + ",";
-
-						} else {
-
-							insValues += "'" + values.get(k) + "'" + ",";
-
-						}
-
-					} else {
-
-						if (teacher.Attrs.get(k).getDatatype() == "INTEGER") {
-
-							insValues += values.get(k);
-
-						} else {
-
-							insValues += "'" + values.get(k) + "'";
-
-						}
-
-					}
-
-				}
-
-				String sql = "INSERT INTO " + teacher.getName() + "(" + attributes + ")" + " VALUES (" + insValues
-						+ ");";
-
-				stmt.executeUpdate(sql);
-
-				stmt.close();
-				c.commit();
-				c.close();
-			} catch (Exception e) {
-				System.err.println(e.getClass().getName() + ": " + e.getMessage());
-				System.exit(0);
-			}
-
-		} else {
-			
-			for (int i = 0; i < valuee.split(separator).length; i++) {
-
-				values.add(valuee.split(separator)[i]);
-
-			}
-
-			try {
-				Class.forName("org.sqlite.JDBC");
-				c = DriverManager.getConnection("jdbc:sqlite:test.db");
-				c.setAutoCommit(false);
-				stmt = c.createStatement();
-
-				// Attributes
-
-				for (int j = 0; j < student.Attrs.size(); j++) {
-
-					if (j < student.Attrs.size() - 1) {
-
-						attributes += student.Attrs.get(j).getName() + ",";
-
-					} else {
-
-						attributes += student.Attrs.get(j).getName();
-
-					}
-
-				}
-
-				// Values
-
-				for (int k = 0; k < values.size(); k++) {
-
-					if (k < values.size() - 1) {
-
-						if (student.Attrs.get(k).getDatatype() == "INTEGER") {
-
-							insValues += values.get(k) + ",";
-
-						} else {
-
-							insValues += "'" + values.get(k) + "'" + ",";
-
-						}
-
-					} else {
-
-						if (student.Attrs.get(k).getDatatype() == "INTEGER") {
-
-							insValues += values.get(k);
-
-						} else {
-
-							insValues += "'" + values.get(k) + "'";
-
-						}
-
-					}
-
-				}
-
-				String sql = "INSERT INTO " + student.getName() + "(" + attributes + ")" + " VALUES (" + insValues
-						+ ");";
-
-				stmt.executeUpdate(sql);
-
-				stmt.close();
-				c.commit();
-				c.close();
-			} catch (Exception e) {
-				System.err.println(e.getClass().getName() + ": " + e.getMessage());
-				System.exit(0);
-			}
-			
-		}
-
-	}
-
-	public boolean delUser(String values, boolean table) {
+	/**
+	 * Löscht einen User wenn Username / Passwort korrekt
+	 * 
+	 * @param values
+	 *            --> String Array mit Username , Passwort
+	 * @return --> True wenn gelöscht, false wenn nicht vorhanden oder Passwort
+	 *         nicht korrekt
+	 */
+
+	public boolean delUser (String[] values) {
+
+		boolean deleted = false;
 		
-		if (table) {
-			
-			selTable = "Lehrer";
-			
-		} else {
-			
-			selTable = "Lernender";
-			
-		}
+		// To be continued
 		
-		if (!checkPossible(values)) {
-			
-			try {
-				Class.forName("org.sqlite.JDBC");
-				c = DriverManager.getConnection("jdbc:sqlite:test.db");
-				c.setAutoCommit(false);
-				stmt = c.createStatement();
-
-				String sql1 = "SELECT Password FROM " + selTable + " WHERE UPPER(Username) = UPPER(" + values.split(separator)[1] + ")";
-
-				ResultSet rsPwd = stmt.executeQuery(sql1);
-				
-				while (rsPwd.next()) {
-					
-					passwd = rsPwd.getString("Password");
-					
-				}
-				
-				rsPwd.close();
-				stmt.close();
-				c.commit();
-				c.close();
-			} catch (Exception e) {
-				System.err.println(e.getClass().getName() + ": " + e.getMessage());
-				System.exit(0);
-			}
-
-			if (values.split(separator)[2].equals(passwd)) {
-				
-				try {
-					Class.forName("org.sqlite.JDBC");
-					c = DriverManager.getConnection("jdbc:sqlite:test.db");
-					c.setAutoCommit(false);
-					stmt = c.createStatement();
-
-					String sql = "DELETE FROM " + selTable + " WHERE Username = " + selUsern;
-
-					stmt.executeUpdate(sql);
-
-					stmt.close();
-					c.commit();
-					c.close();
-				} catch (Exception e) {
-					System.err.println(e.getClass().getName() + ": " + e.getMessage());
-					System.exit(0);
-				}				
-				
-			} else {
-				
-				pwdCorr = false; 
-				
-			}
-
-			return pwdCorr;
-			
-		} else {
-			
-			return false;
-			
-		}
+		return deleted;
 
 	}
 

3c649e1d2855d3232528f79e918b6637500d5e4b
hlucca <hlucca@gmx.net>
2016-04-27 09:05:05 +0200

    Model+Interface erweitert

---
+++ src/application/Model.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import gui.ViewInterface;
+
+public abstract class Model implements ModelInterface {
+	
+	private ArrayList<ViewInterface> myViews = new ArrayList<ViewInterface>();
+
+	@Override
+	public int doAction(String functionName, String paramS) {
+		return doAction(functionName, paramS, 0.0F);
+	}
+
+	@Override
+	public int doAction(String functionName) {
+		return doAction(functionName, null, 0.0F);
+	}
+
+	@Override
+	public void registerView(ViewInterface theView) {
+		myViews.add(theView);
+	}
+
+	public void refreshViews() {
+		Iterator<ViewInterface> it = myViews.iterator();
+		while (it.hasNext()) {
+			ViewInterface v = it.next();
+			v.refreshView();
+			v.notify(); // evtl. nicht nötig
+		}
+	}
+
+}

b65b30edeed4b3186d4f5489fc8818bc7856fb3f
miro-albrecht <albrecht.m@bluewin.ch>
2016-04-27 09:56:39 +0200

    Beautify

--- src/application/ModelInterface.java
+++ src/application/ModelInterface.java
@@ -1,5 +1,6 @@
 package application;
 
+<<<<<<< Upstream, based on branch 'master' of https://github.com/RookStudios/Lernkartei.git
 import gui.ViewInterface;
 
 public interface ModelInterface {
@@ -10,3 +11,13 @@ public interface ModelInterface {
 	public abstract void registerView(ViewInterface theView);
 
 }
+=======
+public interface ModelInterface
+{
+	public int doAction (String functionName, String paramS, double paramD);
+
+	public int doAction (String functionName, String paramS);
+
+	public int doAction (String functionName);
+}
+>>>>>>> 2d675ef Beautify

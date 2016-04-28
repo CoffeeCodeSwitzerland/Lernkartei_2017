6acab6211f5d8416a57f291d8e2f5eb48e8ffb11
Nina-Egger <nina.egger@netabc.ch>
2016-04-28 11:02:18 +0200

    StatisticsView Buttons organisiert

--- src/gui/StatisticsView.java
+++ src/gui/StatisticsView.java
@@ -2,22 +2,47 @@ package gui;
 
 import application.Constants;
 import application.MainController;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class StatisticsView extends View
-{
+{		
+	HBox karteiLayout;
+
 	public StatisticsView(String setName, Stage window, MainController controller)
 	{
 		super (setName, window, controller);
 		
-		AppButton b = new AppButton("Back");
-		b.setOnAction(e -> controller.showMain());
+		
+		// Buttons
+		AppButton zurueck = new AppButton("zurück");
+		AppButton meineKlasse = new AppButton("Meine Klasse");
+		AppButton rangliste = new AppButton("Ranglisten");
 
-		BorderPane bp = new BorderPane();
-		bp.setCenter(b);
-		this.setupScene(new Scene(bp, Constants.OPTIMAL_WIDTH, Constants.OPTIMAL_HEIGHT));
+		// Layout für Controls
+		HBox hBox = new HBox(20);
+		hBox.setAlignment(Pos.CENTER);
+
+		hBox.getChildren().addAll(zurueck, meineKlasse, rangliste);
+
+		karteiLayout = new HBox(20);
+		karteiLayout.setAlignment(Pos.CENTER);
+
+		// Laayout für die Scene
+		BorderPane borderPane = new BorderPane();
+		borderPane.setPadding(new Insets(15));
+
+		borderPane.setCenter(karteiLayout);
+		borderPane.setBottom(hBox);
+
+
+		this.setupScene(new Scene(borderPane, Constants.OPTIMAL_WIDTH, Constants.OPTIMAL_HEIGHT));
+		zurueck.setOnAction(e -> controller.showMain());
+
 	}
 
 	@Override

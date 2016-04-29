a54122d9dacdea6da78e6b2bb60cfc9b91a799ab
Nina-Egger <nina.egger@netabc.ch>
2016-04-29 13:29:19 +0200

    Statistiken hinzugefügt ein Balken Chart

--- src/gui/StatisticsView.java
+++ src/gui/StatisticsView.java
@@ -2,9 +2,16 @@ package gui;
 
 import application.Constants;
 import application.MainController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -12,6 +19,11 @@ import javafx.stage.Stage;
 public class StatisticsView extends View
 {		
 	HBox karteiLayout;
+	final static String austria = "Austria";
+	final static String brazil = "Brazil";
+	final static String france = "France";
+	final static String italy = "Italy";
+	final static String usa = "USA";
 
 	public StatisticsView(String setName, Stage window, MainController controller)
 	{
@@ -22,7 +34,7 @@ public class StatisticsView extends View
 		AppButton zurueck = new AppButton("zurück");
 		AppButton meineKlasse = new AppButton("Meine Klasse");
 		AppButton rangliste = new AppButton("Ranglisten");
-
+		
 		// Layout für Controls
 		HBox hBox = new HBox(20);
 		hBox.setAlignment(Pos.CENTER);
@@ -38,8 +50,45 @@ public class StatisticsView extends View
 
 		borderPane.setCenter(karteiLayout);
 		borderPane.setBottom(hBox);
+		 
+		
+		
+		final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final BarChart<String,Number> bc = 
+            new BarChart<String,Number>(xAxis,yAxis);
+        bc.setTitle("Country Summary");
+        xAxis.setLabel("Country");       
+        yAxis.setLabel("Value");
+ 
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("2003");       
+        series1.getData().add(new XYChart.Data(austria, 25601.34));
+        series1.getData().add(new XYChart.Data(brazil, 20148.82));
+        series1.getData().add(new XYChart.Data(france, 10000));
+        series1.getData().add(new XYChart.Data(italy, 35407.15));
+        series1.getData().add(new XYChart.Data(usa, 12000));      
+        
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("2004");
+        series2.getData().add(new XYChart.Data(austria, 57401.85));
+        series2.getData().add(new XYChart.Data(brazil, 41941.19));
+        series2.getData().add(new XYChart.Data(france, 45263.37));
+        series2.getData().add(new XYChart.Data(italy, 117320.16));
+        series2.getData().add(new XYChart.Data(usa, 14845.27));  
+        
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("2005");
+        series3.getData().add(new XYChart.Data(austria, 45000.65));
+        series3.getData().add(new XYChart.Data(brazil, 44835.76));
+        series3.getData().add(new XYChart.Data(france, 18722.18));
+        series3.getData().add(new XYChart.Data(italy, 17557.31));
+        series3.getData().add(new XYChart.Data(usa, 92633.68));
 
-
+        bc.getData().addAll(series1, series2, series3);
+        
+        karteiLayout.getChildren().addAll(bc);
+        
 		this.setupScene(new Scene(borderPane, Constants.OPTIMAL_WIDTH, Constants.OPTIMAL_HEIGHT));
 		zurueck.setOnAction(e -> controller.showMain());
 

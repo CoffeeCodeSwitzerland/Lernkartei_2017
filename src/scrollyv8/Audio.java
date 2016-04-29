01db08a698872e50177923c3769cf14a75d8eb94
rogerschneiter <roger.schneiter@gmail.com>
2016-04-29 11:48:56 +0200

    Audio play function

---
+++ src/scrollyv8/Audio.java
@@ -0,0 +1,17 @@
+package scrollyv8;
+
+public class Audio {
+	
+	/**
+	 * Standartmässig ist der Pfad folgendermassen: Ordner Sounds / Audiofiles
+	 * 
+	 * @param fileName --> Name des Files dass du abspielen willst
+	 */
+	
+	public static void playAudio(String fileName) {
+		
+		
+		
+	}
+	
+}

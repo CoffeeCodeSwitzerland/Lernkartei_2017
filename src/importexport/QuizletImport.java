4238646f2508ce04876803c5f7a2769ea5141caa
rogerschneiter <roger.schneiter@gmail.com>
2016-04-29 13:59:42 +0200

    Methode importSet added
    In Bearbeitung

--- src/importexport/QuizletImport.java
+++ src/importexport/QuizletImport.java
@@ -1,9 +1,25 @@
 package importexport;
 
+import java.util.ArrayList;
+
+
 public class QuizletImport {
-	
+
 	/**
-	 * TODO: Quizlet Import
+	 * Importiert ein von Quizlet heruntergeladenes Set
+	 * 
+	 * @param set --> Benötigt eine ArrayList<String> mit Quizlet Set
+	 * @return --> True wenns funktioniert hat ,False wenns nicht funktioniert hat
 	 */
-	
+
+	public static boolean importSet (ArrayList<String> set) {
+
+		boolean imported = false;
+		
+		// TODO: Quizlet Import
+		
+		return imported;
+
+	}
+
 }

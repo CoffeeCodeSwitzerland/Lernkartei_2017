7868e1116ffb13873077fe895ee92caad49bcfa0
miro-albrecht <albrecht.m@bluewin.ch>
2016-04-29 09:40:04 +0200

    Kommentiere und strukturiere model Klassen

--- src/models/UserModel.java
+++ src/models/UserModel.java
@@ -5,66 +5,85 @@ import java.util.ArrayList;
 import database.UserCards;
 import user.User;
 
+
 public class UserModel extends DataModel
 {
-
-	public UserModel(String myName)
+	public UserModel (String myName)
 	{
 		super(myName);
 	}
 
 	@Override
-	public int doAction(String functionName, String paramS, double paramD)
+	public int doAction (String functionName, String paramS, double paramD)
 	{
-		//Wenn "Ich bin ein Lehrer" angekreuzt -> 1 als double übergeben sonst 0
-		boolean ifTeacher;
-		if (paramD == 1) {
-			ifTeacher = true;
-		} else 
+		// Wenn "Ich bin ein Lehrer" angekreuzt -> 1 als double übergeben sonst
+		// 0
+		boolean isTeacher;
+		if (paramD == 1)
 		{
-			ifTeacher = false;
+			isTeacher = true;
 		}
-		
-		//Funktionen von "User" 
+		else
+		{
+			isTeacher = false;
+		}
+
+		// Funktionen von "User"
 		if (functionName.equals("register"))
 		{
-			//Um User zu registrieren
-			//Als String bitte Username:::Email:::Password -> ::: = Separator (Constants.SEPARATOR)
-			try {
-			boolean success = User.Register(paramS, ifTeacher);
-			return success ? 1 : -1;
-			} catch (Exception e) {
+			// Um User zu registrieren
+			// Als String bitte Username:::Email:::Password -> ::: = Separator
+			// (Constants.SEPARATOR)
+			try
+			{
+				boolean success = User.Register(paramS, isTeacher);
+				return success ? 1 : -1;
+			}
+			catch (Exception e)
+			{
 				return -2;
 			}
-			//Funktion von User zum Einloggen  
-			//Als String bitte Username:::Password -> ::: = Separator (Constants.SEPARATOR)
-		} else if (functionName.equals("login"))
+			// Funktion von User zum Einloggen
+			// Als String bitte Username:::Password -> ::: = Separator
+			// (Constants.SEPARATOR)
+		}
+		else if (functionName.equals("login"))
 		{
-			try {
-			boolean success = User.Login(paramS);
-			return success ? 1 : -1;
-			} catch (Exception e) {
+			try
+			{
+				boolean success = User.Login(paramS);
+				return success ? 1 : -1;
+			}
+			catch (Exception e)
+			{
 				return -2;
 			}
-			//Funktion zum Löschen von Benutzern -> Nur durchführbar wenn man eingeloggt ist mit dem User, welchen man löschen will
-			//Als String wird hier nur Username nötig
-		} else if (functionName.equals("delete")) {
-			try {
-			boolean success = User.Delete(paramS);
-			return success ? 1 : -1;
-			} catch (Exception e) {
+			// Funktion zum Löschen von Benutzern -> Nur durchführbar wenn man
+			// eingeloggt ist mit dem User, welchen man löschen will
+			// Als String wird hier nur Username nötig
+		}
+		else if (functionName.equals("delete"))
+		{
+			try
+			{
+				boolean success = User.Delete(paramS);
+				return success ? 1 : -1;
+			}
+			catch (Exception e)
+			{
 				return -2;
 			}
-		} else
+		}
+		else
 		{
 			return 0;
 		}
 	}
 
 	@Override
-	public ArrayList<String> getData(String query)
+	public ArrayList<String> getData (String query)
 	{
-		//Returnt alle Karteien des Benutzers in form von einer ArrayList
+		// Returnt alle Karteien des Benutzers in form von einer ArrayList
 		return UserCards.getCards();
 	}
 }

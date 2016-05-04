0be66cdae5dae53e14569f03efb9e066c071b38d
Nina-Egger <nina.egger@netabc.ch>
2016-05-04 09:16:40 +0200

    Impressum wird bearbeitet

--- src/gui/ImpressumView.java
+++ src/gui/ImpressumView.java
@@ -1,7 +1,10 @@
 package gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import mvc.Controller;
 import mvc.FXSettings;
 import mvc.FXView;
@@ -15,14 +18,19 @@ public class ImpressumView extends FXView
 		// Buttons
 		AppButton backBtn = new AppButton("Zurück");
 		
+		//Box für die Buttons
+		HBox hBox = new HBox(20);
+		hBox.getChildren().addAll(backBtn);
+			
 		// Behaviour
 		backBtn.setOnAction(e -> getController().showMain());
 		
 		// Layout
-		BorderPane layout = new BorderPane();
-		layout.setCenter(backBtn);
+		BorderPane borderPane = new BorderPane();
+		borderPane.setPadding(new Insets(15));
+		borderPane.setBottom(hBox);
 		
-		setupScene(new Scene(layout, FXSettings.OPTIMAL_WIDTH, FXSettings.OPTIMAL_HEIGHT));
+		setupScene(new Scene(borderPane, FXSettings.OPTIMAL_WIDTH, FXSettings.OPTIMAL_HEIGHT));
 	}
 
 	@Override

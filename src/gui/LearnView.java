0b36769ddb37b719b1aa6063cc960a19a709b9a3
miro-albrecht <albrecht.m@bluewin.ch>
2016-05-04 15:51:03 +0200

    Füge Testkarte zu LearnView hinzu

--- src/gui/LearnView.java
+++ src/gui/LearnView.java
@@ -1,13 +1,21 @@
 package gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import mvc.Controller;
 import mvc.FXViewModel;
 
 public class LearnView extends FXViewModel
 {
-
+	AppButton successfulBtn = new AppButton("Richtig");
+	AppButton wrongBtn = new AppButton("Falsch");
+	Label headLbl = new Label("");
+	AppButton card = new AppButton("Front");
+	
 	public LearnView (String setName, Controller controller)
 	{
 		super(setName, controller);
@@ -15,15 +23,37 @@ public class LearnView extends FXViewModel
 		AppButton backBtn = new AppButton("Zurück");
 		backBtn.setOnAction(e -> getController().getView("boxview").show());
 		
-		BorderPane layout = new BorderPane(backBtn);
+		headLbl.setId("bold");
 		
-		setupScene(new Scene(layout, 800, 450));
+		card.setMinWidth(160);
+		card.setMinHeight(90);
+		card.setId("bold");
+		card.setOnAction(e -> card.setText(card.getText() == "Test" ? "Nope" : "Test"));
+		
+		HBox controlLayout = new HBox(20);
+		controlLayout.setAlignment(Pos.CENTER);
+		controlLayout.getChildren().addAll(backBtn, successfulBtn, wrongBtn);
+		
+		BorderPane mainLayout = new BorderPane();
+		mainLayout.setPadding(new Insets(15));
+		mainLayout.setTop(headLbl);
+		mainLayout.setCenter(card);
+		mainLayout.setBottom(controlLayout);
+		
+		setupScene(new Scene(mainLayout, 800, 450));
 	}
 
 	@Override
 	public void refreshView ()
 	{
-		
+		if (getData() == null || getData().equals(""))
+		{
+			successfulBtn.setDisable(false);
+			wrongBtn.setDisable(false);
+		}
+		else
+		{
+			headLbl.setText(getData());
+		}
 	}
-
 }

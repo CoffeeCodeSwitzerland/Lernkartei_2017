7ca3e89688884b3559226d1bf80604149f05261a
rogerschneiter <roger.schneiter@gmail.com>
2016-05-04 14:18:54 +0200

    QuizletModel finally created

--- src/models/QuizletModel.java
+++ src/models/QuizletModel.java
@@ -1,9 +1,10 @@
 package models;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
-
+import quizlet.Quizlet;
 import application.Constants;
-import database.Database;
 import mvc.Model;
 
 public class QuizletModel extends Model {
@@ -16,7 +17,35 @@ public class QuizletModel extends Model {
 	@Override
 	public ArrayList<String> getDataList (String query) {
 		
-		
+		if (query.split(Constants.SEPARATOR)[0].equals("search")) {
+			
+			Quizlet q = new Quizlet("3RhaPk5H9C");
+			try {
+				return q.searchSet(query.split(Constants.SEPARATOR)[1]);
+			}
+			catch (MalformedURLException e) {
+				debug.Debugger.out(e.getMessage());
+			}
+			catch (IOException e) {
+				debug.Debugger.out(e.getMessage());
+			}
+			
+		} else if (query.split(Constants.SEPARATOR)[0].equals("set")) {
+			
+			Quizlet q = new Quizlet("3RhaPk5H9C");
+			try {
+				return q.getSet(query.split(Constants.SEPARATOR)[1]);
+			}
+			catch (MalformedURLException e) {
+				debug.Debugger.out(e.getMessage());
+			}
+			catch (IOException e) {
+				debug.Debugger.out(e.getMessage());
+			}
+			
+		} else {
+			return null;			
+		}
 		
 		return null;
 		

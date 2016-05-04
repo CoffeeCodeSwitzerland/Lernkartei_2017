a3437cc13a96170736927b01ef0cfc1db3d41871
Joel-Haeberli <haebu@rubigen.ch>
2016-05-04 16:05:23 +0200

    funzt jetzt wie ich will, wie das ist brauche ich hier nicht zu schreiben

--- src/gui/StatsView.java
+++ src/gui/StatsView.java
@@ -2,16 +2,16 @@ package gui;
 
 import java.util.ArrayList;
 
-import application.Constants;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.layout.HBox;
 import mvc.*;
+import models.*;
 /**
  * Diese Klasse soll die gleiche Funktionalität wie StatisticsView haben und diese dann auch ersetzen
  * Sie soll beliebig viele Säulen generieren
@@ -21,80 +21,59 @@ import mvc.*;
  */
 
 public class StatsView extends FXView
-{
-	//Alle Daten Holen
-	private ArrayList<String> OriginalData = new ArrayList<String>();
+{	
+	//Zugehöriges Model deklarieren und instanzieren
+	ProfilModel Pm = new ProfilModel("heee?");
 	
-	//Alle Daten in zwei Arrays einteilen: Kartei[] und Points[]
-	private String[] Kartei = new String[OriginalData.size()];
-	private Double[] Points = new Double[OriginalData.size()];
+	//Arrays für Daten welche ich von ProfilModel erhalte
+//	private ArrayList<String> Karteien;
+//	private ArrayList<String> Punkte;
 	
-	//X und Y Achsen erstellen
-	final CategoryAxis xAchse = new CategoryAxis();
-	final NumberAxis yAchse = new NumberAxis();
 	
-	//Ein Bar Chart um Diagramm anzeigen zu können
-	private final BarChart<String, Number> stats;
 	
-	//Constructor
+//	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public StatsView(String setName, Controller controller)
 	{
 		super(setName, controller);
-
-		//Chart erstellen
-		stats = new BarChart<String, Number>(xAchse, yAchse);
-		//Titel und Label setzen
-        stats.setTitle("Statistik der Karteien");
-        xAchse.setLabel("Name");       
-        yAchse.setLabel("Punkte");
-
-//        generateTwoArrays();
-//		generateSerie();
-
-		//Box für das Diagramm
-		HBox chartLayout = new HBox(50);
-		chartLayout.setAlignment(Pos.CENTER);
-        //Chart ins KartenLayout (center) einfügen
-
-		// Buttons
-		AppButton zurueck = new AppButton("zurück");
-		// zurueck eine Action hinzufügen
-        zurueck.setOnAction(e -> getController().showMain());
+//		Karteien = Pm.getDataList("karteien");
+//		Punkte = Pm.getDataList("punkte");
 		
-		// Layout für Controls
-		HBox hBox = new HBox(20);
-		hBox.setAlignment(Pos.CENTER);
-		hBox.getChildren().addAll(zurueck);
+		//HBox für das Diagramm
+		HBox Diagram = new HBox();
+		Diagram.setAlignment(Pos.CENTER);
 		
-		BorderPane bp = new BorderPane();
-		bp.getChildren().addAll(chartLayout, hBox, stats);
-
-		setupScene(new Scene(bp, getController().getFXSettings().OPTIMAL_WIDTH, getController().getFXSettings().OPTIMAL_HEIGHT));
-	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void generateSerie() 
-	{
-		//Eine Serie für jede Kartei erstellen
-		for (int i = 0; i < Points.length; i++)
-		{
-			XYChart.Series serie = new XYChart.Series<>();
-			serie.setName(Kartei[i]);
-			serie.getData().add(new XYChart.Data(Kartei[i], Points[i]));
-			stats.getData().addAll(serie);
-		}
-	}
-	
-	public void generateTwoArrays() 
-	{
-		//Aus den Originaldaten zwei Arrays erstellen
-		for (int i = 0; i < OriginalData.size(); i++)
-		{
-			String[] temp = new String[2];
-			temp = OriginalData.get(i).split(Constants.SEPARATOR);
-			Kartei[i] = temp[0];
-			Points[i] = Double.parseDouble(temp[1]);
-		}
+		//HBox für die Buttons / Controls
+		HBox Controls = new HBox();
+		Controls.setAlignment(Pos.BOTTOM_CENTER);
+		
+		//Buttons / Controls
+		AppButton back = new AppButton("Zurück");
+		
+		//*********************************Diagramm Start*********************************//
+		//Achsen erstellen
+		CategoryAxis xAchse = new CategoryAxis();
+		NumberAxis yAchse = new NumberAxis();
+		
+		xAchse.setLabel("Kartei");
+		yAchse.setLabel("Punkte");
+		
+		//BarChart erstellen
+		BarChart<String, Number> bc = new BarChart<String, Number>(xAchse, yAchse);
+		
+		//Eine Serie erstellen
+//		Series serie = new Series();
+//		for (int i = 0; i < Karteien.size(); i++)
+//		{
+//			serie.getData().add(new XYChart.Data(Karteien.get(i), Punkte.get(i)));
+//		}
+		
+		//Neue Szene
+		Scene scene = new Scene(bc);
+//		bc.getData().addAll(serie);
+		
+		//Szene setzen und Buttons Event
+		this.setupScene(scene);
+		back.setOnAction(e -> getController().getMainView());
 	}
 
 	@Override

37e1570a922888b8d78fbe782b59f006b6973d88
miro-albrecht <albrecht.m@bluewin.ch>
2016-05-11 12:43:50 +0200

    Verbessere LearnView

--- src/views/LearnView.java
+++ src/views/LearnView.java
@@ -6,6 +6,7 @@ import controls.Globals;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -25,6 +26,9 @@ public class LearnView extends FXViewModel
 	Label headLbl = new Label("");
 	AppButton card = new AppButton("Front");
 	
+	Button preCard = new Button("\u25C0");
+	Button nextCard = new Button("\u25B6");
+	
 	int counter = 0;
 	
 	String[] cardData = new String[3];
@@ -33,7 +37,7 @@ public class LearnView extends FXViewModel
 	@Override
 	public Parent constructContainer() {
 		AppButton backBtn = new AppButton("Zurück");
-		backBtn.setOnAction(e -> getController().getView("boxview").show());
+		backBtn.setOnAction(e -> {counter = 0; getController().getView("boxview").show();});
 		
 		headLbl.setId("bold");
 		
@@ -54,9 +58,21 @@ public class LearnView extends FXViewModel
 		card.setId("bold");
 		card.setOnAction(e -> card.setText(card.getText().equals(cardData[1]) ? cardData[2] : cardData[1]));
 		
+		
+		
+		preCard.setOnAction(e -> {
+			counter = counter > 0 ? counter - 1 : counter;
+			refreshView();
+		});
+		
+		nextCard.setOnAction(e -> {
+			counter++;
+			refreshView();
+		});
+		
 		HBox controlLayout = new HBox(20);
 		controlLayout.setAlignment(Pos.CENTER);
-		controlLayout.getChildren().addAll(backBtn, successfulBtn, wrongBtn);
+		controlLayout.getChildren().addAll(backBtn, preCard, successfulBtn, wrongBtn, nextCard);
 		
 		BorderPane mainLayout = new BorderPane();
 		mainLayout.setPadding(new Insets(15));
@@ -81,20 +97,21 @@ public class LearnView extends FXViewModel
 			ArrayList<String> cards = getController().getModel("learn").getDataList(getData());
 			if (counter < cards.size())
 			{
+				successfulBtn.setDisable(false);
+				wrongBtn.setDisable(false);
+				nextCard.setDisable(false);
 				String d = cards.get(counter); // Ensure valid counter variable
 				cardData = d.split(Globals.SEPARATOR);
 				card.setText(cardData[1]);
 			}
 			else
 			{
-				card.setDisable(true);
-				boolean goBackToBoxView = Alert.ok("Fertig", "Du hast alle Karten gelernt! Willst du zur Übersicht zurück?");
-				if (goBackToBoxView)
-				{
-					counter = 0;
-					card.setDisable(false);
-					getController().getView("boxview").show();
-				}
+				successfulBtn.setDisable(true);
+				wrongBtn.setDisable(true);
+				nextCard.setDisable(true);
+				card.setText("");
+				cardData = null;
+				counter = cards.size();
 			}
 			
 		}

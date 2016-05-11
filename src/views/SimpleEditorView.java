ebfe8347ddc290ae652ba509b3ab778e6dd527e8
miro-albrecht <albrecht.m@bluewin.ch>
2016-05-11 09:47:59 +0200

    Link to EditorView

--- src/views/SimpleEditorView.java
+++ src/views/SimpleEditorView.java
@@ -2,6 +2,8 @@ package views;
 
 import java.util.ArrayList;
 
+import com.mysql.jdbc.Constants;
+
 import controls.Globals;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -20,26 +22,28 @@ public class SimpleEditorView extends FXViewModel
 {
 	// ArrayList<VBox> cards;
 
-	public SimpleEditorView(String newName, Controller newController) {
+	public SimpleEditorView (String newName, Controller newController)
+	{
 		// this constructor is the same for all view's
 		super(newName, newController);
 		construct();
 	}
 
-	VBox editLayout = new VBox(10);
-	Label headLbl;
+	VBox	editLayout	= new VBox(10);
+	Label	headLbl;
 
 	@Override
-	public Parent constructContainer() {
+	public Parent constructContainer ()
+	{
 		headLbl = new Label("");
 		headLbl.setId("bold");
-		
+
 		AppButton backBtn = new AppButton("Zurück");
 		backBtn.setOnAction(e -> getController().getView("boxview").show());
 
 		BorderPane headLayout = new BorderPane(headLbl);
 		headLayout.setPadding(new Insets(25));
-		
+
 		editLayout.setPadding(new Insets(10));
 		editLayout.setAlignment(Pos.TOP_CENTER);
 
@@ -67,7 +71,7 @@ public class SimpleEditorView extends FXViewModel
 		if (data != null)
 		{
 			headLbl.setText(data + " - " + getController().getView("boxview").getData());
-			
+
 			ArrayList<String> cardStrings = getController().getModel("cards").getDataList(data);
 			ArrayList<HBox> cards = new ArrayList<>();
 			debug.Debugger.out("" + cardStrings.size());
@@ -76,7 +80,7 @@ public class SimpleEditorView extends FXViewModel
 				String[] cardSides = s.split(Globals.SEPARATOR);
 				TextField front = new TextField(cardSides[1]);
 				TextField back = new TextField(cardSides[2]);
-				
+
 				front.focusedProperty().addListener(e ->
 				{
 					if (!front.isFocused())
@@ -84,11 +88,12 @@ public class SimpleEditorView extends FXViewModel
 						if (back.getText() != null && !back.getText().equals("") && front.getText() != null
 								&& !front.getText().equals(""))
 						{
-							getController().getModel("cards").doAction("edit", cardSides[0] + Globals.SEPARATOR + front.getText() + Globals.SEPARATOR + back.getText());
+							getController().getModel("cards").doAction("edit", cardSides[0] + Globals.SEPARATOR
+									+ front.getText() + Globals.SEPARATOR + back.getText());
 						}
 					}
 				});
-				
+
 				back.focusedProperty().addListener(e ->
 				{
 					if (!back.isFocused())
@@ -96,20 +101,27 @@ public class SimpleEditorView extends FXViewModel
 						if (back.getText() != null && !back.getText().equals("") && front.getText() != null
 								&& !front.getText().equals(""))
 						{
-							getController().getModel("cards").doAction("edit", cardSides[0] + Globals.SEPARATOR + front.getText() + Globals.SEPARATOR + back.getText());
+							getController().getModel("cards").doAction("edit", cardSides[0] + Globals.SEPARATOR
+									+ front.getText() + Globals.SEPARATOR + back.getText());
 						}
 					}
 				});
-				
 
 				Button delete = new Button("X");
+				Button editBtn = new Button("\u2055");
 				delete.setMaxWidth(35);
 				delete.setMinWidth(35);
 				delete.setOnAction(e -> getController().getModel("cards").doAction("delete", cardSides[0]));
-
+				editBtn.setMaxWidth(35);
+				editBtn.setMinWidth(35);
+				editBtn.setOnAction(e ->
+				{
+					getController().getView("editorview").setData(front.getText() + Globals.SEPARATOR + back.getText());
+					getController().getView("editorview").show();
+				});
 				HBox v = new HBox(8);
 				v.setAlignment(Pos.CENTER);
-				v.getChildren().addAll(front, back, delete);
+				v.getChildren().addAll(front, back, delete, editBtn);
 				cards.add(v);
 			}
 

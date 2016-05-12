b725217add8ca6e0c73cffeb3d3ab8e1b3c76b4e
rogerschneiter <roger.schneiter@gmail.com>
2016-05-12 15:33:08 +0200

    Updated DB Path

--- src/database/Score.java
+++ src/database/Score.java
@@ -14,7 +14,7 @@ public class Score {
 
 	// URL und Driver
 	private static String	windowsUser	= debug.Environment.getUserName();
-	private static String	url			= "jdbc:sqlite:" + windowsUser + ".db";
+	private static String	url			= "jdbc:sqlite:" + debug.Environment.getUserPath() + "\\" + windowsUser + ".db";
 	private static String	driver		= "org.sqlite.JDBC";
 
 	/**

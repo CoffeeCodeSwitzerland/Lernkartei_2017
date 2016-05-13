bdff1789cd7c704f2cc8a60baad78063c5855733
rogerschneiter <roger.schneiter@gmail.com>
2016-05-13 08:07:00 +0200

    Global added

--- src/controls/Globals.java
+++ src/controls/Globals.java
@@ -7,5 +7,6 @@ public abstract class Globals {
 	public static final String appVersion = "[V0.1 alpha]";
 	public static final String SEPARATOR = ":::";
 	public static final String db_Path = "WISS_Learncards_db";
+	public static final String db_name = "Lernkarten_db";
 
 }

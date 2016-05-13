76f9bf1940823a30abad2d6099ef44b58c4fc560
miro-albrecht <albrecht.m@bluewin.ch>
2016-05-13 14:32:33 +0200

    Fixe Probleme mit WebView
    Sieht leider immer noch schiesse aus

--- src/views/OptionsView.java
+++ src/views/OptionsView.java
@@ -3,6 +3,8 @@ package views;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import mvc.Controller;
 import mvc.FXView;
@@ -25,17 +27,19 @@ public class OptionsView extends FXView
 	@Override
 	public Parent constructContainer ()
 	{
-		//Label cardLimitDescription = new Label("Anzahl Karten, die auf einmal gelernt werden limitieren.");
+		Label cardLimitDescription = new Label("Anzahl Karten, die auf einmal gelernt werden, limitieren.");
+		TextField cardLearnLimit = new TextField("");
 		AppButton back = new AppButton("_Zurück");
 
-		
+		cardLimitDescription.setMaxWidth(200);
+		cardLimitDescription.setWrapText(true);
+		cardLearnLimit.setMaxWidth(200);
 		
 		VBox mainLayout = new VBox();
 		mainLayout.setPadding(new Insets(10));
 		mainLayout.setSpacing(10);
 		mainLayout.setAlignment(Pos.CENTER);
-		//mainLayout.getChildren().add(cardLimitDescription);
-		mainLayout.getChildren().add(back);
+		mainLayout.getChildren().addAll(cardLimitDescription, cardLearnLimit, back);
 
 		back.setOnAction(e -> getController().showMainView());
 

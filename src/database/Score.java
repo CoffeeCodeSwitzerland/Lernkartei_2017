bf83423008227bc7e480a3175bd1dde8ef71b497
rogerschneiter <roger.schneiter@gmail.com>
2016-05-18 09:23:45 +0200

    Fixed Error

--- src/database/Score.java
+++ src/database/Score.java
@@ -1,5 +1,6 @@
 package database;
-//z
+
+// z
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -15,12 +16,12 @@ import javafx.collections.ObservableList;
 public class Score {
 
 	// URL und Driver
-	
-	private static String	url			= "jdbc:sqlite:" +  debug.Environment.getDatabasePath()
-										 + controls.Globals.db_name + ".db";
-	private static String	driver		= "org.sqlite.JDBC";
-	
-	private static Integer anzahlLeben;
+
+	private static String	url		= "jdbc:sqlite:" + debug.Environment.getDatabasePath()
+			+ controls.Globals.db_name + ".db";
+	private static String	driver	= "org.sqlite.JDBC";
+
+	private static Integer	anzahlLeben;
 
 	/**
 	 * 
@@ -31,9 +32,8 @@ public class Score {
 	 * @return --> Returned einen Double Wert des Scores, returned -1, wenn kein
 	 *         Score vorhanden
 	 */
-	
-	public Score() 
-	{
+
+	public Score () {
 		Connection c = null;
 		Statement stmt = null;
 
@@ -47,8 +47,9 @@ public class Score {
 					+ "Score REAL NOT NULL);";
 
 			stmt.executeUpdate(sql);
-			
-		} catch (Exception e) {
+
+		}
+		catch (Exception e) {
 			Debugger.out(e.getMessage());
 		}
 	}
@@ -164,12 +165,19 @@ public class Score {
 		return true;
 
 	}
-	//getScores gibt alle Pnuktzahlen zu den Kartien zurück. In der ArrayList sind Stringarrays gelistet, welche wie folgt aufgebaut sind:
-	// | 0 KarteiName | 1 Punktzahl | (!Achtung! -> Die Punktzahl wird als String übergeben -> Sie sollte am Schluss ein Double sein)
-	//Später wenn Online-implementierung dazukommt, kann man als Parameter den Username angeben um die richtige Funktionalität dieser Funktion sicher zu stellen
-	//Wenn ein Fehler autritt, wird in catch ein Array in die Liste gespeichert, in welchem zwei Fehler stehen. Im 1. Eintrag eine Message für Entwickler 
-	//und im 2. füe den Benutzer, welcher einfacher lesbar ist, da der User nicht weiss was er mit der 1. Anfangen soll.
-	
+	// getScores gibt alle Pnuktzahlen zu den Kartien zurück. In der ArrayList
+	// sind Stringarrays gelistet, welche wie folgt aufgebaut sind:
+	// | 0 KarteiName | 1 Punktzahl | (!Achtung! -> Die Punktzahl wird als
+	// String übergeben -> Sie sollte am Schluss ein Double sein)
+	// Später wenn Online-implementierung dazukommt, kann man als Parameter den
+	// Username angeben um die richtige Funktionalität dieser Funktion sicher zu
+	// stellen
+	// Wenn ein Fehler autritt, wird in catch ein Array in die Liste
+	// gespeichert, in welchem zwei Fehler stehen. Im 1. Eintrag eine Message
+	// für Entwickler
+	// und im 2. füe den Benutzer, welcher einfacher lesbar ist, da der User
+	// nicht weiss was er mit der 1. Anfangen soll.
+
 	private static ArrayList<String> allScores = new ArrayList<String>();
 
 	public ArrayList<String> getScores () {
@@ -183,7 +191,7 @@ public class Score {
 			Class.forName(driver);
 			c = DriverManager.getConnection(url);
 			stmt = c.createStatement();
-			
+
 			String all = "SELECT Kartei, Score FROM Score";
 			ResultSet rs = stmt.executeQuery(all);
 
@@ -204,12 +212,13 @@ public class Score {
 
 		return allScores;
 	}
-	
+
 	ObservableList<String> Ranks = FXCollections.observableArrayList();
-	public ObservableList<String> getRanking() {
-		
+
+	public ObservableList<String> getRanking () {
+
 		Ranks.clear();
-		
+
 		Connection c = null;
 		Statement stmt = null;
 
@@ -217,7 +226,7 @@ public class Score {
 			Class.forName(driver);
 			c = DriverManager.getConnection(url);
 			stmt = c.createStatement();
-			
+
 			String all = "SELECT Kartei FROM Score ORDER BY Score ASC";
 			ResultSet rs = stmt.executeQuery(all);
 
@@ -231,12 +240,12 @@ public class Score {
 			String allgemeinerFehler = "Es tut uns leid aber wir konnten ihre Karteien nicht finden";
 			Ranks.add(allgemeinerFehler);
 		}
-		
+
 		return Ranks;
 	}
-	
-	public void generateTestdata(String Insert) {
-		
+
+	public void generateTestdata (String Insert) {
+
 		Connection c = null;
 		Statement stmt = null;
 
@@ -244,15 +253,16 @@ public class Score {
 			Class.forName(driver);
 			c = DriverManager.getConnection(url);
 			stmt = c.createStatement();
-			//TestData in die Score Tabelle einschreiben
+			// TestData in die Score Tabelle einschreiben
 			stmt.executeUpdate(Insert);
 			System.out.println(Insert);
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	public void dropTestData(String Drop) {
+
+	public void dropTestData (String Drop) {
 		Connection c = null;
 		Statement stmt = null;
 
@@ -260,27 +270,28 @@ public class Score {
 			Class.forName(driver);
 			c = DriverManager.getConnection(url);
 			stmt = c.createStatement();
-			//Tabelle löschen
+			// Tabelle löschen
 			stmt.executeUpdate(Drop);
 			System.out.println(Drop);
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 
-	private static String genString(String Kartei, String Score)
-	{
+	private static String genString (String Kartei, String Score) {
 		return Kartei + Globals.SEPARATOR + Score;
 	}
-	
+
 	/**
 	 * 
-	 * Wenn eine Karte richtig war, wird mit dieser Funktion die Anzahl richtiger Karten um 1 erhöht
+	 * Wenn eine Karte richtig war, wird mit dieser Funktion die Anzahl
+	 * richtiger Karten um 1 erhöht
 	 * 
 	 */
-	
-	public static void correctCard() {
-		
+
+	public static void correctCard () {
+
 		Connection c = null;
 		Statement stmt = null;
 
@@ -295,21 +306,26 @@ public class Score {
 
 			debug.Debugger.out(sql);
 			stmt.executeUpdate(sql);
-			
+
 			Integer currentLifes = 0;
-			
+
 			c.setAutoCommit(false);
-			
+
 			String getCurrent = "SELECT Lifecount FROM Lifes";
+
+			ResultSet getCurt = stmt.executeQuery(getCurrent);
+
+			if (getCurt.next()) {
+				currentLifes = getCurt.getInt("Lifecount");
+				getCurt.close();
+			}
 			
-			ResultSet rs = stmt.executeQuery(getCurrent);
-			currentLifes = rs.getInt("Lifecount");
-			
+			getCurt.close();
 			c.setAutoCommit(true);
 
 			String updt = "UPDATE Lifes SET Lifecount = " + (currentLifes + 1);
 			stmt.executeUpdate(updt);
-			
+
 			stmt.close();
 			c.close();
 
@@ -317,12 +333,12 @@ public class Score {
 		catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
-		}	
-		
+		}
+
 	}
-	
-	public static int getLifecount() {
-		
+
+	public static int getLifecount () {
+
 		Connection c = null;
 		Statement stmt = null;
 
@@ -337,19 +353,19 @@ public class Score {
 
 			debug.Debugger.out(sql);
 			stmt.executeUpdate(sql);
-			
+
 			Integer currentLifes = 0;
-			
+
 			c.setAutoCommit(false);
-			
+
 			String getCurrent = "SELECT Lifecount FROM Lifes";
-			
+
 			ResultSet rs = stmt.executeQuery(getCurrent);
 			currentLifes = rs.getInt("Lifecount");
-			
+
 			anzahlLeben = 0;
 			anzahlLeben = currentLifes % 30;
-			
+
 			stmt.close();
 			c.close();
 
@@ -357,14 +373,14 @@ public class Score {
 		catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
-		}	
-		
+		}
+
 		return anzahlLeben;
-		
+
 	}
-	
+
 	public static void death () {
-		
+
 		Connection c = null;
 		Statement stmt = null;
 
@@ -379,21 +395,21 @@ public class Score {
 
 			debug.Debugger.out(sql);
 			stmt.executeUpdate(sql);
-			
+
 			Integer currentLifes = 0;
-			
+
 			c.setAutoCommit(false);
-			
+
 			String getCurrent = "SELECT Lifecount FROM Lifes";
-			
+
 			ResultSet rs = stmt.executeQuery(getCurrent);
 			currentLifes = rs.getInt("Lifecount");
-			
+
 			c.setAutoCommit(true);
 
 			String updt = "UPDATE Lifes SET Lifecount = " + (currentLifes - 30);
 			stmt.executeUpdate(updt);
-			
+
 			stmt.close();
 			c.close();
 
@@ -402,7 +418,7 @@ public class Score {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
 		}
-		
+
 	}
 
 }

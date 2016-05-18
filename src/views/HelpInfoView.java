08a099b4b0f81fe6eab7f2c368b38a74a12acaa9
Nina-Egger <nina.egger@netabc.ch>
2016-05-18 13:26:08 +0200

    Added Back Buttons Jetzt kommt man auch wieder zurück

--- src/views/HelpInfoView.java
+++ src/views/HelpInfoView.java
@@ -1,6 +1,12 @@
 package views;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import mvc.fx.FXController;
 import mvc.fx.FXView;
 
@@ -22,7 +28,54 @@ public class HelpInfoView extends FXView
 	@Override
 	public Parent constructContainer() {
 		// TODO Auto-generated method stub
-		return null;
+		
+				// Buttons
+				AppButton backBtn = new AppButton("_Zurück");
+					
+				//Labels (für die Infotexte)
+				Label labelTitel = new Label("Anleitung");
+				
+				//Hier kommt der angezeigte Text hin:
+				Label labelText = new Label("Erste Schritte\nWichtiges:\nKontakte:\netc...");
+				
+				//Zeilenumbruch am Fensterrand
+				labelText.setWrapText(true);
+
+				//IDs für CSS
+				labelTitel.setId("impressumtext");
+				labelText.setId("impressumtext");
+
+				//Damit der Text nicht bis zum Fensterrand geht sondern noch etwas abstand hat
+				Double size = getController().getMyFXStage().getOPTIMAL_WIDTH()*.95;
+				size += 10;
+				labelText.setPrefWidth(size);
+				labelTitel.setPrefWidth(size);
+				
+				//Box für die Navigation
+				HBox naviBox = new HBox(10);
+				naviBox.getChildren().addAll(backBtn);
+					
+				//Box für Titel
+				VBox TitelBox = new VBox(10);
+				TitelBox.getChildren().addAll(labelTitel);
+				TitelBox.setAlignment(Pos.CENTER);
+						
+				//Box für Mitte Text
+				VBox BoxMitText = new VBox(20);
+				BoxMitText.getChildren().addAll(labelText);
+				BoxMitText.setAlignment(Pos.CENTER);
+				
+				// Behaviour
+				backBtn.setOnAction(e -> getController().showMainView());
+				
+				// Layout
+				BorderPane borderPane = new BorderPane();
+				borderPane.setPadding(new Insets(15));
+				borderPane.setBottom(naviBox);
+				borderPane.setCenter(BoxMitText);
+				borderPane.setTop(TitelBox);
+				
+		return borderPane;
 	}
 
 	@Override

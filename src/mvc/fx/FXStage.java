53b4c6e4f82b66937a354cdb76a3bdd172455bac
hlucca <hlucca@gmx.net>
2016-05-18 16:05:35 +0200

    FX Stage hat kein CSS mehr s. Globals

--- src/mvc/fx/FXStage.java
+++ src/mvc/fx/FXStage.java
@@ -19,20 +19,11 @@ public class FXStage extends Stage {
 	private double OPTIMAL_WIDTH  = 920;
 	private double OPTIMAL_HEIGHT = 570;
 	private Color preferredBackroundColor = new Color(1.0,1.0,1.0,0.5); 
-	private static String stylePath = "styles\\style.css";
 
 	public FXStage (Stage refToPrimaryStage) {
 		primaryStage = refToPrimaryStage;
 	}
 	
-	public void setStylePath(String stylePath) {
-		FXStage.stylePath = stylePath;
-	}
-
-	public String getStylePath() {
-		return FXStage.stylePath;
-	}
-
 	public Stage getStage() {
 		if (this.primaryStage == null) {
 			return this;

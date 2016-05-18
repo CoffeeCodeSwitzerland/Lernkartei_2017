9576cf6872a4536e716b5e97b769ca3e324828a7
david-schor <david.schor2000@gmail.com>
2016-05-18 15:16:41 +0200

    BBCode(Italic) möglich

--- src/views/LearnView.java
+++ src/views/LearnView.java
@@ -144,15 +144,18 @@ public class LearnView extends FXViewModel
 				nextCard.setDisable(false);
 				String d = cards.get(counter); // Ensure valid counter variable
 				cardData = d.split(Globals.SEPARATOR);
-				if(cardData[2].contains("[b]") || cardData[1].contains("[b]")){
+				if(cardData[2].contains("[b]") || cardData[1].contains("[b]") || cardData[2].contains("[i]") || cardData[1].contains("[i]")){
 					cardData[2] = cardData[2].replace("[b]", "<b>");
 					cardData[1] = cardData[1].replace("[b]", "<b>");
+					cardData[2] = cardData[2].replace("[i]", "<i>");
+					cardData[1] = cardData[1].replace("[i]", "<i>");
 				}
-				if(cardData[2].contains("[/b]") || cardData[1].contains("[/b]")){
+				if(cardData[2].contains("[/b]") || cardData[1].contains("[/b]") || cardData[2].contains("[/i]") || cardData[1].contains("[/i]")){
 					cardData[2] = cardData[2].replace("[/b]", "</b>");
 					cardData[1] = cardData[1].replace("[/b]", "</b>");
+					cardData[2] = cardData[2].replace("[/i]", "</i>");
+					cardData[1] = cardData[1].replace("[/i]", "</i>");
 				}
-				//cardData = d.split(Globals.SEPARATOR);
 				engine.loadContent(cardData[1]);
 				frontIsShowed = true;
 			}

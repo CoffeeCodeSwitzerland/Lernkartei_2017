6905a99da1af7c64eb2700764bc0b89672d90fed
miro-albrecht <albrecht.m@bluewin.ch>
2016-05-18 13:44:40 +0200

    Erste Version von preLearnView

--- src/views/PreLearnView.java
+++ src/views/PreLearnView.java
@@ -1,10 +1,12 @@
 package views;
 
 import debug.Debugger;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import mvc.View;
 import mvc.fx.FXController;
 import mvc.fx.FXViewModel;
 
@@ -31,6 +33,7 @@ public class PreLearnView extends FXViewModel
 	{
 		getController().getModel("learn").getDataList(null).clear();
 		getController().getModel("learn").setString(null);
+		mainLayout.setPadding(new Insets(50));
 		return mainLayout;
 	}
 
@@ -46,10 +49,31 @@ public class PreLearnView extends FXViewModel
 		Label stackName = new Label(getData());
 		stackName.setId("bold");
 		
-		Label stackInfo = new Label("Anzahl Karten: " + getController().getModel("learn").getDataList(getData()).size());
+		Label stackInfo = new Label("Anzahl Karten: " + getFXController().getModel("learn").getDataList(getData()).size());
+		
+		String score = "";
+		if (getFXController().getModel("statistics") != null)
+		{
+			if (getFXController().getModel("statistics").getDataList(getData()) != null)
+			{
+				score = getFXController().getModel("statistics").getDataList(getData()).get(0);
+			}
+		}
+		
+		Label stackScore = new Label("Fortschritt: " + score);
+		
+		AppButton goBtn = new AppButton("Go");
+		goBtn.setOnAction(e -> {
+			View v = getFXController().getView("learnview");
+			v.setData(getData());
+			v.show();
+		});
+		
+		AppButton backBtn = new AppButton("Zurück");
+		backBtn.setOnAction(e -> getFXController().getView("stack").show());
 		
 		VBox layout = new VBox(30);
-		layout.getChildren().addAll(stackName, stackInfo);
+		layout.getChildren().addAll(stackName, stackInfo, stackScore, goBtn, backBtn);
 		
 		mainLayout.setCenter(layout);
 	}

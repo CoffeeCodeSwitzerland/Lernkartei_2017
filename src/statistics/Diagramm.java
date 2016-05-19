2555c3fe30265c497b40f7b5f183a3b8f618f0c6
Joel-Haeberli <haebu@rubigen.ch>
2016-05-19 15:27:48 +0200

    PUSH for Hugo

--- src/statistics/Diagramm.java
+++ src/statistics/Diagramm.java
@@ -21,17 +21,13 @@ public class Diagramm
 	}
 	
 	public ArrayList<Double> getPunkte()
-	{				
-		Debugger.out("Eingang getPunkte");
+	{		
 		for (int i = 0; i < Stacks.size(); i++)
 		{
-			Debugger.out("Stack : " + Stacks.get(i));
 			Double[] temp = Database.getScore(Stacks.get(i).toString());
-			Debugger.out("temp[0] : " + temp[0] + " / " + "temp[1] : " + temp[1]);
 			Double result = (100 / temp[0]) * temp[1];
 			Debugger.out("Resultat : " + result);
 			Punkte.add(result);
-			Debugger.out("Ende For-Durchlauf : " + i);
 		}		
 		return Punkte;
 	}
--- src/statistics/Rangliste.java
+++ src/statistics/Rangliste.java
@@ -1,16 +1,89 @@
 package statistics;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import database.Categories;
+import database.Database;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Rangliste
 {
+	
+	public Rangliste() {
+		getKarteien();
+		getPunkte();
+		sortKarteien();
+	}
+
+	Categories C = new Categories();
+	
+	ArrayList<String> NamesAndPoints = new ArrayList<String>();
+	ArrayList<Double> Punkte = new ArrayList<Double>();
+	ArrayList<String> Stacks = new ArrayList<String>();
 
 	public ObservableList<String> getRangliste()
 	{
 		ObservableList<String> Ranking = FXCollections.observableArrayList();
-		
+		Ranking.addAll(NamesAndPoints);
 		return null;
 	}
+	
+	public void getKarteien() {
+		Stacks = C.getStacknames();
+	}
+	
+	private void getPunkte() {
+		for (int i = 0; i < Stacks.size(); i++)
+		{
+			Double[] temp = Database.getScore(Stacks.get(i).toString());
+			Punkte.add(temp[1]);
+		}
+	}
+	
+	private void sortKarteien() {
+		
+		ArrayList<Double> tempPunkte = new ArrayList<Double>();
+		ArrayList<String> tempSortedStacks = new ArrayList<String>();
 
+		Boolean isFirstSet = false;
+		/*for (int i = 0; i < array.length; i++)
+		{
+			if (isFirstSet) {
+				
+			} else {
+				tempPunkte
+				isFirstSet = true;
+			}
+		}*/
+	}
+
+	/*private static String url = "jdbc:sqlite:" + controls.Environment.getDatabasePath() + controls.Globals.db_name + ".db";
+	private static String driver = "org.sqlite.JDBC";
+
+	private void getStacks()
+	{
+
+		Connection c = null;
+		Statement stmt = null;
+
+		try
+		{
+			Class.forName(driver);
+			c = DriverManager.getConnection(url);
+			stmt = c.createStatement();
+			
+			String searchStacks = "SELECT ";
+			ResultSet rs = stmt.executeQuery(searchStacks);
+			
+		} catch (Exception e)
+		{
+
+		}
+
+	}*/
 }

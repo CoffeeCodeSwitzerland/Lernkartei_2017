df8b6774d784c5f58635a137e5e13294cde2206b
Nina-Egger <nina.egger@netabc.ch>
2016-05-20 09:47:02 +0200

    Added ScrollPane to Quizlet Info(Test)

--- src/views/QuizletInfoView.java
+++ src/views/QuizletInfoView.java
@@ -4,6 +4,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -19,52 +21,37 @@ public class QuizletInfoView extends FXView
 	}
 
 	@Override
-	public Parent constructContainer() {
-		// Buttons
-		AppButton backBtn = new AppButton("_Zurück");
-		
-		
-		
+	public Parent constructContainer ()
+	{
 		//Labels (für die Infotexte)
 		Label labelTitel = new Label("Quizlet Info");
-		Label labelText = new Label("Bla bla bla...");
+		Label labelText = new Label("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
 		labelText.setWrapText(true);
 
 		labelTitel.setId("impressumtext");
 		labelText.setId("impressumtext");
+		
+		AppButton backBtn = new AppButton("_Zurück");
 
-		Double size = getController().getMyFXStage().getOPTIMAL_WIDTH()*.95;
-		size += 10;
-		labelText.setPrefWidth(size);
-		labelTitel.setPrefWidth(size);
+		BorderPane headLayout = new BorderPane(labelTitel);
+		headLayout.setPadding(new Insets(25));
+		ScrollPane scroller = new ScrollPane();
 		
-		//Box für die Navigation
-		HBox naviBox = new HBox(10);
-		naviBox.getChildren().addAll(backBtn);
-			
-		//Box für Titel
-		VBox TitelBox = new VBox(10);
-		TitelBox.getChildren().addAll(labelTitel);
-		TitelBox.setAlignment(Pos.CENTER);
-				
-		//Box für Mitte Text
-		VBox BoxMitText = new VBox(20);
-		BoxMitText.getChildren().addAll(labelText);
-		BoxMitText.setAlignment(Pos.CENTER);
+		scroller.setContent(labelText);
+		scroller.setHbarPolicy(ScrollBarPolicy.NEVER);
+		scroller.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 		
-		// Behaviour
-		backBtn.setOnAction(e -> getController().showMainView());
-		
-		// Layout
-		BorderPane borderPane = new BorderPane();
-		borderPane.setPadding(new Insets(15));
-		borderPane.setBottom(naviBox);
-		borderPane.setCenter(BoxMitText);
-		borderPane.setTop(TitelBox);
-			
-		
-		// TODO Auto-generated method stub
-		return borderPane;
+		HBox controlLayout = new HBox(20);
+		controlLayout.setAlignment(Pos.BOTTOM_LEFT);
+		controlLayout.getChildren().addAll(backBtn);
+
+		BorderPane mainLayout = new BorderPane();
+		mainLayout.setPadding(new Insets(15));
+		mainLayout.setTop(headLayout);
+		mainLayout.setCenter(scroller);
+		mainLayout.setBottom(controlLayout);
+
+		return mainLayout;
 	}
 
 	@Override

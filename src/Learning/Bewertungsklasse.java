cc29aa4fa4cfb0d34552031df7ec00a31b9be258
yanisweibel <yanis12344@gmail.com>
2016-05-20 14:29:11 +0200

    formated

--- src/Learning/Bewertungsklasse.java
+++ src/Learning/Bewertungsklasse.java
@@ -13,15 +13,13 @@ public class Bewertungsklasse {
 
 		ArrayList<String[]> cards = Database.pullFromStock(query);
 
-		
-
 		if (cards == null) {
 			debug.Debugger.out("getData cards = null");
 			return result;
 		} else {
-			
-			//new lerndatenzuweisung(cards);
-			
+
+			// TODO new lerndatenzuweisung(cards);
+
 			while (result.size() < cards.size()) {
 				Integer i = (int) (Math.random() * 50000) % cards.size();
 
@@ -31,11 +29,9 @@ public class Bewertungsklasse {
 
 				zufallsZahlen.add(i);
 				String tempResult = cards.get(i)[0] + Globals.SEPARATOR + cards.get(i)[1] + Globals.SEPARATOR
-						+ cards.get(i)[2] + Globals.SEPARATOR
-						+ cards.get(i)[3] ;
+						+ cards.get(i)[2] + Globals.SEPARATOR + cards.get(i)[3];
 				result.add(tempResult);
 			}
-			
 
 			return result;
 		}

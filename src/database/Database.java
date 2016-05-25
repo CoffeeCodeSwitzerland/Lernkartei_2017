5fd8e6294f7fca922e51fc4e752c7e482f1e3361
rogerschneiter <roger.schneiter@gmail.com>
2016-05-25 13:31:10 +0200

    Color Attribut entfernt.
    Ersetzt durch Datum

--- src/database/Database.java
+++ src/database/Database.java
@@ -3,7 +3,6 @@ package database;
 import java.sql.*;
 import java.util.ArrayList;
 
-
 public class Database {
 
 	// Varibeln Connection
@@ -17,7 +16,7 @@ public class Database {
 	 * 
 	 * @param values
 	 *            --> Array mit 5 Werten: 1. Vorderseite, 2. Rückseite, 3.
-	 *            Set_ID, 4. Priorität (1-5), 5. Color
+	 *            Set_ID, 4. Priorität (1-5), 5. Datum
 	 */
 
 	public static boolean pushToStock (String[] values) {
@@ -37,7 +36,7 @@ public class Database {
 					" Set_ID    		INTEGER NOT NULL, " +
 					" Priority	    INTEGER DEFAULT 1," +
 					" Description    TEXT    		, " +
-					" Color			TEXT    		 )";
+					" Datum			TEXT    		 )";
 
 			debug.Debugger.out(sql);
 			stmt.executeUpdate(sql);
@@ -61,7 +60,7 @@ public class Database {
 
 			c.setAutoCommit(true);
 
-			String insert = "INSERT INTO Stock (Frontside, Backside, Set_ID, Priority, Color)" +
+			String insert = "INSERT INTO Stock (Frontside, Backside, Set_ID, Priority, Datum)" +
 					"VALUES ('" + values[0] + "','" + values[1] + "'," + setID + ", " + values[3] + ", '"
 					+ values[4] + "')";
 
@@ -104,7 +103,7 @@ public class Database {
 					" Set_ID    		INTEGER NOT NULL, " +
 					" Priority	    INTEGER DEFAULT 1," +
 					" Description    TEXT    		, " +
-					" Color			TEXT    		 )";
+					" Datum			TEXT    		 )";
 
 			debug.Debugger.out(sql);
 			stmt.executeUpdate(sql);
@@ -137,7 +136,7 @@ public class Database {
 				set[3] = rs.getString("Description");
 				set[4] = Integer.toString(rs.getInt("Set_ID"));
 				set[5] = Integer.toString(rs.getInt("Priority"));
-				set[6] = rs.getString("Color");
+				set[6] = rs.getString("Datum");
 				results.add(set);
 
 			}

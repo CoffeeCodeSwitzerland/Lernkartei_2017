e8a1c1e51f821ae397c2d60452d4255e01fbadb0
miro-albrecht <albrecht.m@bluewin.ch>
2016-05-26 11:10:01 +0200

    RenameView look better

--- src/views/RenameView.java
+++ src/views/RenameView.java
@@ -3,16 +3,33 @@ package views;
 import java.util.ArrayList;
 
 import globals.Globals;
+import javafx.beans.Observable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import mvc.fx.FXController;
 import mvc.fx.FXViewModel;
-import views.components.AppButton;
+import views.components.BackButton;
 
+
+/**
+ * Flexible View, die das Umbenennen von Doors und Stacks erlaubt.
+ * 
+ * @author miro albrecht
+ *
+ */
 public class RenameView extends FXViewModel
 {
+
+	VBox	elements	= new VBox(20);
+	String	oldValue	= "";
+	Observable widthEvent;
 
 	public RenameView (String setName, FXController newController)
 	{
@@ -20,33 +37,58 @@ public class RenameView extends FXViewModel
 		construct(setName);
 	}
 
-	VBox elements = new VBox(20);
-	String oldValue = "";
-	
 	@Override
 	public Parent constructContainer ()
 	{
-		AppButton backBtn = new AppButton("Zurück");
-		backBtn.setOnAction(e -> getFXController().showLastView());
+		BackButton backBtn = new BackButton(getFXController());
+		backBtn.setOnAction(e -> getWindow().getScene().widthProperty().removeListener(event ->
+		{
+			elements.setMinWidth(getWindow().getScene().getWidth() - 80);
+		}));
+
+		HBox controlsLayout =  new HBox();
+		controlsLayout.setPadding(new Insets(20, 0, 0, 0));
+		controlsLayout.getChildren().add(backBtn);
 		
-		BorderPane mainLayout = new BorderPane(elements);
-		mainLayout.setBottom(backBtn);
-		
+		elements.setPadding(new Insets(30));
+		elements.setAlignment(Pos.TOP_CENTER);
+
+		ScrollPane scrollLayout = new ScrollPane(elements);
+		scrollLayout.setHbarPolicy(ScrollBarPolicy.NEVER);
+		scrollLayout.setPadding(new Insets(10));
+
+		BorderPane mainLayout = new BorderPane(scrollLayout);
+		mainLayout.setBottom(controlsLayout);
+		mainLayout.setPadding(new Insets(30));
+
+		getFXController().getModel("door").registerView(this);
+		getFXController().getModel("stack").registerView(this);
+
 		return mainLayout;
 	}
 
 	@Override
 	public void refreshView ()
 	{
+		elements.setMinWidth(getWindow().getScene().getWidth() - 80);
+		
+		getWindow().getScene().widthProperty().addListener(event ->
+		{
+			elements.setMinWidth(getWindow().getScene().getWidth() - 80);
+		});
+
+		// info[0] = stack OR door (model)
+		// info[1] = doors OR stacks of which door (getDataList command)
+
 		String[] info = getData().split(Globals.SEPARATOR);
 		ArrayList<String> list = getFXController().getModel(info[0]).getDataList(info[1]);
-		
+
 		elements.getChildren().clear();
-		
+
 		for (String s : list)
 		{
 			TextField field = new TextField(s);
-			field.focusedProperty().addListener(e ->
+			field.focusedProperty().addListener(event ->
 			{
 				if (field.isFocused())
 				{

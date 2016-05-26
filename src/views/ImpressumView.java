232800a196856f1296d7506e12f71c3bc647fcfe
Nina-Egger <nina.egger@netabc.ch>
2016-05-26 08:11:35 +0200

    Links aktiviert für WISS/BIT/Unsere Webseite

--- src/views/ImpressumView.java
+++ src/views/ImpressumView.java
@@ -1,13 +1,16 @@
 package views;
 
+import globals.Functions;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import mvc.fx.FXController;
 import mvc.fx.FXView;
 import views.components.AppButton;
@@ -24,13 +27,28 @@ public class ImpressumView extends FXView
 	@Override
 	public Parent constructContainer() {
 		Label labelTitel = new Label("Impressum");
-		Label labelText = new Label("Das Programm WISSLearnCards wurde programmiert im Rahmen eines Projekts durch die Informatiklernenden\n im Auftrag der\n\nStiftung Wirtschaftsinformatikschule Schweiz WISS\nOstermundigenstrasse 81\nCH-3006 Bern\nhttp://www.wiss.ch/\n\nTeilnehmer:\n\nInformatiklernende vom\n\nBundesamt für Informatik und Telekommunikation BIT\nMonbijoustrasse 74\nCH-3003 Bern\nhttps://www.bit.admin.ch/\n\n•	Yanis Weibel\n•	Joel Häberli\n•	David Schor\n•	Nina Egger\n•	Roger Schneiter\n•	Miro Albrecht\n•	Tim Leibacher\nhttp://bund2015.wiss-bern.ch/\n");
+		Label labelText = new Label("Das Programm WISSLearnCards wurde programmiert im Rahmen eines Projekts"
+				+ " durch die Informatiklernenden\n im Auftrag der\n\nStiftung Wirtschaftsinformatikschule Schweiz "
+				+ "WISS\nOstermundigenstrasse 81\nCH-3006 Bern\n\n\nTeilnehmer:\n\nInformatiklernende "
+				+ "vom\n\nBundesamt für Informatik und Telekommunikation BIT\nMonbijoustrasse 74\nCH-3003 Bern\n\n\n•	"
+				+ "Yanis Weibel\n•	Joel Häberli\n•	David Schor\n•	Nina Egger\n•	Roger Schneiter\n•	Miro Albrecht\n•	"
+				+ "Tim Leibacher\n\n");
 		labelText.setWrapText(true);
 		labelText.setMaxWidth(800);
 
 		labelTitel.setId("impressumtitel");
 		labelText.setId("impressumtext");
-			
+		
+		Hyperlink WISSlink = new Hyperlink("WISS Webseite");
+		WISSlink.setOnAction(e -> Functions.openWebpage("http://www.wiss.ch/"));
+		
+		Hyperlink BITLink = new Hyperlink("BIT Webseite");
+		WISSlink.setOnAction(e -> Functions.openWebpage("https://www.bit.admin.ch/"));
+		
+		Hyperlink LehrlingeLink = new Hyperlink("Unsere Webseite");
+		WISSlink.setOnAction(e -> Functions.openWebpage("http://bund2015.wiss-bern.ch/"));
+		
+		
 		AppButton backBtn = new AppButton("_Zurück");
 		backBtn.setOnAction(e -> getController().showMainView());
 
@@ -39,7 +57,10 @@ public class ImpressumView extends FXView
 		ScrollPane scroller = new ScrollPane();
 		scroller.setMaxWidth(800);
 				
-		scroller.setContent(labelText);
+		VBox contentLayout = new VBox(20);
+		contentLayout.getChildren().addAll(labelText, WISSlink, BITLink, LehrlingeLink);
+		
+		scroller.setContent(contentLayout);
 		scroller.setHbarPolicy(ScrollBarPolicy.NEVER);
 		scroller.setVbarPolicy(ScrollBarPolicy.ALWAYS);  
 			

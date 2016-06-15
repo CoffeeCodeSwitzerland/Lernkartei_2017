86a837f34079f3f721129f39b14929c9f6e2ac57
miro-albrecht <albrecht.m@bluewin.ch>
2016-06-15 10:47:04 +0200

    Prohibit use of old doAction() from StackModel

--- src/models/StackModel.java
+++ src/models/StackModel.java
@@ -3,70 +3,23 @@ package models;
 import java.util.ArrayList;
 
 import database.Stack;
+import debug.Supervisor;
 import mvc.Model;
 
 
 public class StackModel extends Model
 {
+	/**
+	 * @deprecated
+	 */
 	@Override
 	public int doAction (String functionName, String paramS, double paramD)
 	{
-		// +0: Falsche Parameter in doAction
-		// +1: Erfolgreich erstellt
-		// +2: Erfolgreich gelöscht
-		// -1: Keine Tür mit dem Namen
-		// -2: Kategorie bereits vorhanden
-		// -3: Konnte nicht gelöscht werden
-
-		// -4: Leerer String
-
-		// possible --> 1 = Nicht vorhanden, -1 = vorhanden
-
-		if (functionName.equals("new"))
-		{
-			String eingabe[] = paramS.split(globals.Globals.SEPARATOR);
-			if (eingabe.length < 2) { return -4; }
-			int i = database.Stack.newStack(eingabe[1], eingabe[0]);
-			refreshViews();
-			return i;
-		}
-		else if (functionName.equals("delete"))
-		{
-			if (database.Stack.delStack(paramS))
-			{
-				refreshViews();
-				return 2;
-			}
-			else
-			{
-				refreshViews();
-				return -3;
-			}
-		}
-		else if (functionName.equals("possible"))
-		{
-			if (database.Stack.possible(paramS))
-			{
-				return 1;
-			}
-			else
-			{
-				return -1;
-			}
-		}
-		else if (functionName.equals("update"))
-		{
-			if (database.Stack.update(paramS.split(globals.Globals.SEPARATOR)[0], paramS.split(globals.Globals.SEPARATOR)[1]))
-			{
-				return 1;
-			}
-			else
-			{
-				return -1;
-			}
-		}
-
-		return 0;
+		// deprecated Method
+		
+		Supervisor.errorAndDebug(this, "WARNIG (StackModel): Use of deprecated method doAction(). Use the new one!");
+		
+		return -9;
 	}
 
 	@Override

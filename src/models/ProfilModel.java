57f286491ef9c50f421750f7c9278564dd09f548
miro-albrecht <albrecht.m@bluewin.ch>
2016-06-15 10:43:16 +0200

    Update ProfilModel (doAction())

--- src/models/ProfilModel.java
+++ src/models/ProfilModel.java
@@ -1,49 +1,69 @@
 package models;
-//z
+
 import java.util.ArrayList;
 
 import database.UserCards;
-import globals.Globals;
+import debug.Debugger;
+import debug.Supervisor;
 import javafx.collections.ObservableList;
 import mvc.fx.FXModel;
 import user.Profil;
 import user.User;
 
+/**
+ * 
+ * @author joel häberli
+ *
+ */
 public class ProfilModel extends FXModel
 {
-
-//	public ProfilModel(String myName)
-//	{
-//		super(myName);
-//	}
-
 	//Wenn man Username ändern will bitte als paramS ein String in der Form newName:::oldName übergeben
-	private static String[] Data = new String[2];
 	@Override
 	public int doAction(String functionName, String paramS, double paramD)
 	{
 		if (functionName.equals("change"))
 		{
 			//Ändert den Usernamen, wenn der neue nicht bereits vorhanden ist
-			getArray(paramS);
-			boolean success = User.setUsername(Data[0], Data[1]);
-			return success ? 1 : -1;
-		} else if (functionName.equals("getName"))
+			
+			Supervisor.errorAndDebug(this, "ProfilModel: Method deprecated! Please use the new doAction!");
+			
+			return -1;
+		}
+		else if (functionName.equals("getName"))
 		{
 			//Gibt den Usernamen des Users zurück
-			User.getUsername();
-			return 1;
-		} else
+			Supervisor.errorAndDebug(this, "ProfilModel: Method deprecated! Please use the new doAction!");
+			
+			return -1;
+		}
+		else
 		{
 			//Defaultreturn
-			return -2;
+			return 0;
 		}
 	}
-
-	private void getArray(String paramS)
+	
+	@Override
+	public int doAction(Command command, String... param)
 	{
-		Data = paramS.split(Globals.SEPARATOR);
-	}
+		switch (command)
+		{
+			case UPDATE:
+				if (param.length != 2) { return -2; }
+				
+				boolean success = User.setUsername(param[0], param[1]);
+				return success ? 1 : -1;
+				
+			case GET:
+				if (param.length != 0) { Debugger.out("INFO (ProfilModel) : No params are needed"); }
+				
+				User.getUsername();
+				return 1;
+				
+			default:
+				return super.doAction(command, param);
+		}
+	};
 
 	Profil p = new Profil();
 	//Als Query muss man die unten gebrauchten ausdrücke eingeben, damit die gewünschte Funktion aufgerufen wird

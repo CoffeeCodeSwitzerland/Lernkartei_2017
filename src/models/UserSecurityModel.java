7a73c6844bddd256ec1a715eb4719b115546f484
Joel-Haeberli <haebu@rubigen.ch>
2016-06-16 13:21:36 +0200

    UserSecurityModel initialized
    Wird zum einloggen und registrieren verwendet.

--- src/models/UserSecurityModel.java
+++ src/models/UserSecurityModel.java
@@ -1,13 +1,46 @@
 package models;
 
-import mvc.ModelInterface.Command;
+import debug.Logger;
+import multiuser.CannotPerformOperationException;
+import multiuser.User;
 import mvc.fx.FXModel;
 
 public class UserSecurityModel extends FXModel
 {
 	@Override
-	public int doAction (Command command, String... param)
+	public int doAction(Command command, String... param)
 	{
+		switch (command) {
+		case NEW:
+			if (param.length != 4)
+			{
+				return -2;
+			}
+			User newUser = new User();
+			return newUser.registration(param[0], param[1], param[2], param[3]);
+		case DELETE:
+			if (param.length != 2)
+			{
+				return -2;
+			}
+			User u = new User();
+			boolean success = u.delete(param[0], param[1]);
+			return success ? 1 : -1;
+		case TRUE:
+			if (param.length != 2)
+			{
+				return -2;
+			}
+			User b = new User();
+			try
+			{
+				boolean successb = b.login(param[0], param[1]);
+				return successb ? 1 : -1;
+			} catch (CannotPerformOperationException e)
+			{
+				Logger.log(e.getMessage());
+			}
+		}
 		return 1;
 	}
 }
--- src/multiuser/User.java
+++ src/multiuser/User.java
@@ -8,127 +8,192 @@ public class User
 	/*
 	 * @author Joel Häberli
 	 */
-	
-	//Alle Angaben die ein User hat
+
+	// Alle Angaben die ein User hat
 	private String Name;
 	private String Hash;
 	private byte[] Salz;
 	private String Email;
 	private Integer Usertype;
-	//Das system mit klassen wird ev. nicht implementiert aus Zeitgründen
+	// Das system mit klassen wird ev. nicht implementiert aus Zeitgründen
 	private ObservableList<String> myGroups = FXCollections.observableArrayList();
-	
-	//Wird beim registrieren aufgerufen -> Wenn etwas falsch ist oder etwas nicht funktionierte wird false returnt
-	// schritte beim registrieren:
-	// 1.) Die Eingaben mit einem Regex überprüfen
-	// 2.) Salz generieren
-	// 3.) Passwort hashen mit Salz
-	// 4.) Salz, Passworthash, Username, Email in DB speichern
-	public boolean registration(String name, String mail, String passwort, int usertype) {
-		if (checkNameMachbarkeit(name) && checkEmailMachbarkeit(mail) && checkPasswortMachbarkeit(passwort)) {
-			try {
+
+	/** 
+	 * Wird beim registrieren aufgerufen -> Wenn etwas falsch ist oder etwas
+	 * nicht funktionierte wird false returnt
+	 * 
+	 * schritte beim registrieren:
+	 * 1.) Die Eingaben mit einem Regex überprüfen
+	 * 2.) Salz generieren
+	 * 3.) Passwort hashen mit Salz
+	 * 4.) Salz, Passworthash, Username, Email in DB speichern
+	 * 
+	 * @param name -> Username den der User angibt 
+	 * @param mail -> Mail den der User angibt
+	 * @param passwort -> Das gewünschte PW des Users
+	 * @param usertype -> Ob es sich um einen normalen User handelt oder nicht, wird momentan nicht gebraucht
+	 * 
+	 * @return  1 Aktion erfolgreich<br>
+	 *         -1 kein int<br>
+	 *         -2 Eingaben entsprechen nicht dem Regex <br>
+	 *         -3 Der Hash konnte nicht erzeugt werden
+	 */
+	public int registration(String name, String mail, String passwort, String usertype)
+	{
+		try
+		{
+			int ut = Integer.parseInt(usertype); // TODO : Fehler behandlung
+		} catch (Exception e)
+		{
+			return -1;
+		}
+		if (checkNameMachbarkeit(name) && checkEmailMachbarkeit(mail) && checkPasswortMachbarkeit(passwort))
+		{
+			try
+			{
 				setSalt();
 				Hash = Security.createHash(passwort, Salz);
-				return true;
-			} catch (Exception e) {
+				return 1;
+			} catch (Exception e)
+			{
 				Logger.log(e.getMessage());
-				return false;
+				return -3;
 			}
-		} else {
-			return false;
+		} else
+		{
+			return -2;
 		}
 	}
 	
-	// Wird beim Login aufgerufen
-	// Schritte beim Login:
-	// 1.) Salz und Passworthash aus DB laden wo name gleich name welcher übergeben wurde
-	// 2.) Salz und übergebenes Passwort hashen 
-	// 3.) Diesen generierten hash mit Passworthash vergleichen
-	// 4.) Wenn übereinstimmt -> Namen des Users in lokale DB speichern und dann das Profil anzeigen 
-	public boolean login(String name, String passwort) throws CannotPerformOperationException {
+	/**
+	 * 	 Wird beim Login aufgerufen
+	 *   Schritte beim Login:
+	 *   1.) Salz und Passworthash aus DB laden wo name gleich name welcher
+	 *       übergeben wurde
+	 *   2.) Salz und übergebenes Passwort hashen
+	 *   3.) Diesen generierten hash mit Passworthash vergleichen
+	 *   4.) Wenn übereinstimmt -> Namen des Users in lokale DB speichern und dann
+	 *       das Profil anzeigen
+	 * 
+	 * @param name -> Username 
+	 * @param passwort -> Passwort
+	 * @return -> Wenn true Login korrekt / Wenn false Login nicht korrekt
+	 * @throws CannotPerformOperationException -> ist eine Exception, welche aufgrund der Klasse Security existiert.
+	 */
+	public boolean login(String name, String passwort) throws CannotPerformOperationException
+	{
 		Salz = getSalt();
 		Hash = getHash();
-		
-		if (Security.createHash(passwort, Salz) == Hash) {
-			//TODO : Save to local db
+
+		if (Security.createHash(passwort, Salz) == Hash)
+		{
+			// TODO : Save to local db
 			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	// wird beim Löschen von einem Account gebraucht
-	// Löscht den User aus der Db mit all seinen Einträgen
-	public boolean delete(String name, String mail) {
-		return false;
-	}
-	
-	//TODO : Wenn datenbank funktioniert darin speichern
-	private void setSalt() {
-		Salz = Security.generateSalt();
-	}
-	
-	//TODO : Wenn Datenbank funktioniiert Salt daraus auslesen
-	@SuppressWarnings("unused")
-	private byte[] getSalt() {
-		return Salz = null;	
-	}	
-	
-	//Validierung der drei Eingaben mit entsprechendem Regex
-	private Boolean checkNameMachbarkeit(String name) {
-		String regexName = "[A-Za-z\\d](4,100)";
-		if (name.matches(regexName)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	private Boolean checkEmailMachbarkeit(String mail) {
-		String regexMail = "[A-Za-z\\d]{@}[A-Za-z\\d]{\\.}[A-Za-z0-9]";
-		if (mail.matches(regexMail)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	private Boolean checkPasswortMachbarkeit(String passwort) {
-		String regexPw = "[A-Za-z\\d\\!\\?\\$\\*\\%\\&\\£\\@\\'](8,100)";
-		if (passwort.matches(regexPw)) {
-			return true;
-		} else {
+		} else
+		{
 			return false;
 		}
 	}
 
-	//Getter und Setter der Angaben
+	/**
+	 * wird beim Löschen von einem Account gebraucht.
+	 * Löscht den User aus der Db mit all seinen Einträgen
+	 * 
+	 * @param name -> des Eingeloggten User
+	 * @param mail -> des Eingeloggten User
+	 * @return Wenn true konnte der User erfolgreich gelöscht werden / Wenn false nicht.
+	 */
+	public boolean delete(String name, String mail)
+	{
+		return false;
+	}
+
+	// TODO : Wenn datenbank funktioniert darin speichern
+	private void setSalt()
+	{
+		Salz = Security.generateSalt();
+	}
+
+	// TODO : Wenn Datenbank funktioniiert Salt daraus auslesen
+	@SuppressWarnings("unused")
+	private byte[] getSalt()
+	{
+		return Salz = null;
+	}
+
+	// Validierung der drei Eingaben mit entsprechendem Regex
+	private Boolean checkNameMachbarkeit(String name)
+	{
+		String regexName = "[A-Za-z\\d](4,100)";
+		if (name.matches(regexName))
+		{
+			return true;
+		} else
+		{
+			return false;
+		}
+	}
+
+	private Boolean checkEmailMachbarkeit(String mail)
+	{
+		String regexMail = "[A-Za-z\\d]{@}[A-Za-z\\d]{\\.}[A-Za-z0-9]";
+		if (mail.matches(regexMail))
+		{
+			return true;
+		} else
+		{
+			return false;
+		}
+	}
+
+	private Boolean checkPasswortMachbarkeit(String passwort)
+	{
+		String regexPw = "[A-Za-z\\d\\!\\?\\$\\*\\%\\&\\£\\@\\'](8,100)";
+		if (passwort.matches(regexPw))
+		{
+			return true;
+		} else
+		{
+			return false;
+		}
+	}
+
+	// Getter und Setter der Angaben
 	public String getName()
 	{
 		return Name;
 	}
-	//Wenn true -> gespeichert / Wenn false -> nicht gespeicher weil Fehler (User eine Meldung geben)
-	//TODO : Wenn datenbank funktioniert darin speichern
+
+	// Wenn true -> gespeichert / Wenn false -> nicht gespeicher weil Fehler
+	// (User eine Meldung geben)
+	// TODO : Wenn datenbank funktioniert darin speichern
 	public Boolean setName(String name)
 	{
-		if (checkNameMachbarkeit(name)) {
+		if (checkNameMachbarkeit(name))
+		{
 			Name = name;
 			return true;
-		} else {
+		} else
+		{
 			return false;
 		}
 	}
-	//Wenn true -> gespeichert / Wenn false -> nicht gespeicher weil Fehler (User eine Meldung geben)
-	//TODO : Wenn datenbank funktioniert darin speichern
+
+	// Wenn true -> gespeichert / Wenn false -> nicht gespeicher weil Fehler
+	// (User eine Meldung geben)
+	// TODO : Wenn datenbank funktioniert darin speichern
 	public Boolean setPasswort(String passwort)
 	{
-		//Wenn Passwort zulässig und das Salz gefüllt
-		if (checkPasswortMachbarkeit(passwort) && !(Salz == null)) {
+		// Wenn Passwort zulässig und das Salz gefüllt
+		if (checkPasswortMachbarkeit(passwort) && !(Salz == null))
+		{
 			try
 			{
-				if (Security.createHash(passwort, Salz) == null) {
+				if (Security.createHash(passwort, Salz) == null)
+				{
 					return false;
-				} else {
+				} else
+				{
 					Hash = passwort;
 					return true;
 				}
@@ -137,41 +202,53 @@ public class User
 				e.printStackTrace();
 				return false;
 			}
-		} else {
+		} else
+		{
 			return false;
 		}
 	}
+
 	// TODO : Wenn Datenbank funktioniert, den Passwort daraus holen
-	private String getHash() {
+	private String getHash()
+	{
 		return Hash = null;
 	}
+
 	public String getEmail()
 	{
 		return Email;
 	}
-	//Wenn true -> gespeichert / Wenn false -> nicht gespeicher weil Fehler (User eine Meldung geben)
-	//TODO : Wenn datenbank funktioniert darin speichern
+
+	// Wenn true -> gespeichert / Wenn false -> nicht gespeicher weil Fehler
+	// (User eine Meldung geben)
+	// TODO : Wenn datenbank funktioniert darin speichern
 	public Boolean setEmail(String email)
 	{
-		if (checkEmailMachbarkeit(email)) {
+		if (checkEmailMachbarkeit(email))
+		{
 			Email = email;
 			return true;
-		} else {
+		} else
+		{
 			return false;
 		}
 	}
+
 	public Integer getUsertype()
 	{
 		return Usertype;
 	}
+
 	public void setUsertype(Integer usertype)
 	{
 		Usertype = usertype;
 	}
+
 	public ObservableList<String> getMyGroups()
 	{
 		return myGroups;
 	}
+
 	public void setMyGroups(ObservableList<String> myGroups)
 	{
 	}

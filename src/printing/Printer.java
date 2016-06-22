4f32fa8fe2892d921b024df2c4fe14c7849a5fa8
yanisweibel <yanis12344@gmail.com>
2016-06-22 07:26:54 +0200

    organized imports

--- src/printing/Printer.java
+++ src/printing/Printer.java
@@ -3,12 +3,8 @@
  */
 package printing;
 
-import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Graphics;
 import java.awt.PrintJob;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;

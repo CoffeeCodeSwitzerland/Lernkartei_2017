4be30bd1a6f3d83b8d706c6a5464ff6a5be42803
Frithjof Hoppe <frithjof.hoppe@icloud.com>
2017-07-07 09:27:59 +0200

    Created Class Group.java in Package serverdb.usermgmt

---
+++ src/serverdb/usermgmt/Group.java
@@ -0,0 +1,26 @@
+package serverdb.usermgmt;
+
+import java.util.ArrayList;
+
+public class Group 
+{
+	public ArrayList<String> getGroupMember(String name)
+	{
+		return null;
+	}
+	
+	public int getIdByGroup(String name)
+	{
+		return 0;
+	}
+	
+	public String getGroupnameById(int id)
+	{
+		return null;	
+	}
+	
+	public String getGroupOwner(String name)
+	{
+		return name;	
+	}
+}

adcd0346824a5ead608f77147f0caa1b2c0626e4
kruettlip <philippe.kruettli@gmx.ch>
2017-07-07 12:33:36 +0200

    Formatted loginview

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -27,76 +27,78 @@ public class LoginView extends FXView
 		super(newController);
 		construct(newName);
 	}
-	
+
 	BorderPane bp = new BorderPane();
 
 	VBox AllFields;
 	HBox Controls;
 	HBox Errorbox;
-	
+
 	Label errorText;
 
 	TextField txtName;
 	PasswordField pwPassword;
-	
+
 	HomeButton home;
 	AppButton reg;
 	AppButton log;
-	
+
 	@Override
 	public Parent constructContainer()
 	{
 		bp.setId("loginviewbg");
-		
+
 		Errorbox = new HBox(50);
 		Errorbox.setAlignment(Pos.CENTER);
 		Errorbox.setPadding(new Insets(20));
-		
+
 		AllFields = new VBox(50);
 		AllFields.setAlignment(Pos.CENTER);
 		AllFields.setMaxWidth(300);
 		AllFields.setPadding(new Insets(20));
-		
+
 		Controls = new HBox(50);
 		Controls.setAlignment(Pos.CENTER);
 		Controls.setPadding(new Insets(20));
-		
+
 		txtName = new TextField();
 		txtName.setPromptText("Username");
-		
+
 		pwPassword = new PasswordField();
-		pwPassword.setPromptText("Passwort");;
-		
+		pwPassword.setPromptText("Passwort");
+		;
+
 		home = new HomeButton(getFXController());
 		reg = new AppButton("Noch kein Profil?");
 		log = new AppButton("Login");
-		
+
 		errorText = new Label("");
 		Errorbox.getChildren().addAll(errorText);
-		
+
 		Controls.getChildren().addAll(home, reg);
-		
+
 		AllFields.getChildren().addAll(txtName, pwPassword, log);
-		
+
 		bp.setTop(Errorbox);
 		bp.setCenter(AllFields);
 		bp.setBottom(Controls);
-		
+
 		reg.setOnAction(e -> getFXController().showView("registerview"));
-		log.setOnAction(e -> {
-			
-			//So that you can always check again who was logged in last,
-			//but if user logged in, it should be set as ""
+		log.setOnAction(e ->
+		{
+
+			// So that you can always check again who was logged in last,
+			// but if user logged in, it should be set as ""
 			Globals.lastRegisteredUser = "";
-			
+
 			String name = txtName.getText();
 			String password = pwPassword.getText();
-			
-			//So that entries don't exist after leaving the view
+
+			// So that entries don't exist after leaving the view
 			txtName.clear();
 			pwPassword.clear();
-			
-			if(name.equals("user@mail.com") && password.equals("gibbiX12345"))
+
+			if (name.equals("user@mail.com") && password.equals("gibbiX12345"))
 			{
 				Globals.username = name;
 				getFXController().showView("managementselectionview");
@@ -105,46 +107,52 @@ public class LoginView extends FXView
 				Alert alert = new Alert(AlertType.ERROR);
 				alert.setTitle("Fehler 0x0001");
 				alert.setHeaderText("Login-Daten inkorrekt.");
-				alert.setContentText("Benutzername und Passwort stimmen nicht überein oder existieren nicht.\nStellen Sie sicher, dass Ihre Eingaben korrekt sind.");
+				alert.setContentText(
+						"Benutzername und Passwort stimmen nicht überein oder existieren nicht.\nStellen Sie sicher, dass Ihre Eingaben korrekt sind.");
 				pwPassword.clear();
 				txtName.clear();
 				alert.showAndWait();
 			}
 		});
-		
+
 		return bp;
 	}
 
 	@Override
 	public void refreshView()
 	{
-		if(Globals.username.equals(""))
+		if (Globals.username.equals(""))
 		{
-		bp.setId("loginviewbg");
-		
-		if(!Globals.lastRegisteredUser.equals(""))
-		{
-			txtName.setText(Globals.lastRegisteredUser);
-		}
+			bp.setId("loginviewbg");
 
-		//Zuerst überprüfen ob Felder gefüllt
-		//Dann überprüfen ob Eingaben korrekt
-		//Als drittes überprüfen, ob der User existiert oder nicht
-		//Wenn alles klappt noch in die Datenbank speichern und zum Schluss in den UserView wechseln
-		if (!(txtName.equals(null) && txtName.equals("") && pwPassword.equals(null) && pwPassword.equals(""))) {
-			if (txtName.equals("mama")) {
-				getFXController().showView("userview");
-			} else {
-				errorText = new Label("Die Eingaben entsprechen nicht unseren Richtlinien");
+			if (!Globals.lastRegisteredUser.equals(""))
+			{
+				txtName.setText(Globals.lastRegisteredUser);
 			}
-		} else {
-			errorText = new Label("Sie müssen die Felder ausfüllen");
-		}
+
+			// Zuerst überprüfen ob Felder gefüllt
+			// Dann überprüfen ob Eingaben korrekt
+			// Als drittes überprüfen, ob der User existiert oder nicht
+			// Wenn alles klappt noch in die Datenbank speichern und zum Schluss
+			// in den UserView wechseln
+			if (!(txtName.equals(null) && txtName.equals("") && pwPassword.equals(null) && pwPassword.equals("")))
+			{
+				if (txtName.equals("mama"))
+				{
+					getFXController().showView("userview");
+				} else
+				{
+					errorText = new Label("Die Eingaben entsprechen nicht unseren Richtlinien");
+				}
+			} else
+			{
+				errorText = new Label("Sie müssen die Felder ausfüllen");
+			}
 		} else
 		{
 			getFXController().showView("managementselectionview");
 		}
-		
+
 	}
 
 }
